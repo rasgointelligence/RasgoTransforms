d38f369ac1c51454a9e0ff294636ff3ee39bbccc
Patrick <patrick@rasgoml.com>
2021-10-08 11:36:16 -0400

    update lag for jinja

--- Transforms/Column Ops/rasgo_lag.sql
+++ Transforms/Column Ops/rasgo_lag.sql
@@ -1,3 +1,10 @@
--- args: {{Feature}}, {{Amount}}, {{Partition}}, {{OrderBy}}
 
-lag({{Feature}}, {{Amount}}) over (partition by {{Partition}} order by {{OrderBy}}) as Lag_{{Feature}}_{{Amount}}
+
+select
+    {%- for col in Columns -%}
+        {%- for amount in Amounts -%}
+            lag({{col}}, {{amount}}) over (partition by {{Partition}} order by {{OrderBy}}) as Lag_{{col}}_{{amount}},
+        {%- endfor -%}
+    {%- endfor -%}
+    *
+from {{source_table}}
