580abb5e5fe0b82c392fe4448f6f614ee68930a7
Patrick <patrick@rasgoml.com>
2021-10-13 22:23:45 -0400

    fix syntax and initialize var

--- Transforms/column_operations/rasgo_datetrunc.sql
+++ Transforms/column_operations/rasgo_datetrunc.sql
@@ -1,6 +1,7 @@
 -- Args: {{date_part}}, {{date_column}}
 -- date_part: ['year','month','day','week','quarter','hour','minute','second','millisecond','microsecond']
 
+{% set date_list = None %}
 {%- if date_column is string -%}
     {% set date_list = [date_column] %}
 {% else %}
@@ -11,4 +12,4 @@ SELECT *,
 {%- for col in date_list -%}
     DATE_TRUNC({{date_part}}, {{col}}) as {{col}}_{{date_part}} {{ ", " if not loop.last else "" }}
 {%- endfor -%}
-from {{source_table}}
+from {{ source_table }}
