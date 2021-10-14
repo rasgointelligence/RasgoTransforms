dfbb1288b94da5eeed9762dae11ae0098dbceff1
Patrick <patrick@rasgoml.com>
2021-10-13 22:30:00 -0400

    fix syntax

--- Transforms/column_operations/rasgo_lag.sql
+++ Transforms/column_operations/rasgo_lag.sql
@@ -1,10 +1,10 @@
 -- args: {{Columns}}, {{Amounts}}, {{Partition}}, {{OrderBy}}
 
-select
+SELECT
     {%- for col in Columns -%}
         {%- for amount in Amounts -%}
             lag({{col}}, {{amount}}) over (partition by {{Partition}} order by {{OrderBy}}) as Lag_{{col}}_{{amount}},
         {%- endfor -%}
     {%- endfor -%}
     *
-from {{source_table}}
+from {{ source_table }}
