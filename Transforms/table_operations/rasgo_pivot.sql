7bee9661f185e0a769949c92f468a10b6ebe3c06
Patrick <patrick@rasgoml.com>
2021-10-14 11:24:18 -0400

    updated to use dynamic distinct vals

--- Transforms/table_operations/rasgo_pivot.sql
+++ Transforms/table_operations/rasgo_pivot.sql
@@ -1,12 +1,13 @@
 -- args: {{dimensions}}, {{pivot_column}}, {{value_column}}, {{agg_method}}
 
-{% set get_distinct_vals %}
+{%- set get_distinct_vals -%}
     select distinct {{ value_column }}
     from {{ source_table }}
-{% endset %}
-{% set results = run_query(get_distinct_vals) %}
-{% set distinct_vals = results['TICKER'].to_list() %}
+    limit 1000
+{%- endset -%}
+{%- set results = run_query(get_distinct_vals) -%}
+{%- set distinct_vals = results[results.columns[0]].to_list() -%}
 
-SELECT {{dimensions}}, {{ distinct_vals | join(", ") }}
-FROM ( select {{dimensions}}, {{pivot_column}}, {{value_column}} from {{source_table}})
-PIVOT ( {{agg_method}} ( {{pivot_column}} ) FOR {{value_column}} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{dimensions}}, {{ distinct_vals | join(", ") }}
+SELECT {{ dimensions }}, {{ distinct_vals | join(", ") }}
+FROM ( SELECT {{ dimensions }}, {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
+PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{ dimensions }}, {{ distinct_vals | join(", ") }} )
