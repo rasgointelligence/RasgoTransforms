f57b6ba03d6f6f677c235dc0bfa27f60a252ecd2
Patrick <patrick@rasgoml.com>
2021-10-13 23:09:47 -0400

    add new run_query utility

--- Transforms/table_operations/rasgo_pivot.sql
+++ Transforms/table_operations/rasgo_pivot.sql
@@ -1,3 +1,12 @@
--- args: {{dimensions}}, {{list_of_vals}}, {{list_of_vals_no_quote}}, {{pivot_column}}, {{value_column}}, {{agg_method}}
+-- args: {{dimensions}}, {{pivot_column}}, {{value_column}}, {{agg_method}}
 
-SELECT {{dimensions}}, {{list_of_vals_no_quote}} FROM ( select {{dimensions}}, {{pivot_column}}, {{value_column}} from {{source_table}}) PIVOT ( {{agg_method}} ( {{pivot_column}} ) FOR {{value_column}} IN ( {{list_of_vals}} ) ) as p ( {{dimensions}}, {{list_of_vals_no_quote}})
+{% set get_distinct_vals %}
+    select distinct {{ value_column }}
+    from {{ source_table }}
+{% endset %}
+{% set results = run_query(get_distinct_vals) %}
+{% set distinct_vals = results['TICKER'].to_list() %}
+
+SELECT {{dimensions}}, {{ distinct_vals | join(", ") }}
+FROM ( select {{dimensions}}, {{pivot_column}}, {{value_column}} from {{source_table}})
+PIVOT ( {{agg_method}} ( {{pivot_column}} ) FOR {{value_column}} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{dimensions}}, {{ distinct_vals | join(", ") }}
