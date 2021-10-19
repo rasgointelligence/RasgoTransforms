19a7c81fd0e4f93b3dcbf5b032c7830fb9a7b04e
Alex Wolf <alex.r.wolf22@gmail.com>
2021-10-19 14:37:14 -0500

    union jinja updates

--- table_operations/rasgo_filter/rasgo_union.sql
+++ table_operations/rasgo_filter/rasgo_union.sql
@@ -7,26 +7,26 @@
         {%- set database, schema, table = source_table_fqtn.split('.') -%}
     {%- else -%}
         {%- set database, schema, table = rasgo_source_ref(source_id).split('.') -%}
-    {% endif -%}
+    {%- endif -%}
         SELECT COLUMN_NAME FROM {{ database }}.information_schema.columns
         WHERE TABLE_CATALOG = '{{ database }}'
         AND   TABLE_SCHEMA = '{{ schema }}'
         AND   TABLE_NAME = '{{ table }}'
 {%- endmacro -%}
+
 {# Get all Columns in Source Table #}
 {%- set col_names_source_df = run_query(get_source_col_names(source_table_fqtn=source_table)) -%}
 {%- set source_col_names = col_names_source_df['COLUMN_NAME'].to_list() -%}
-{# Get all columns in inputted Source #}
+
+{# Get all columns in Inputted Source #}
 {%- set col_names_other_source_df = run_query(get_source_col_names(source_id=source_id)) -%}
 {%- set other_source_col_names = col_names_other_source_df['COLUMN_NAME'].to_list() -%}
-{# Get colnames which occur across source_table and other source #}
-{%- set union_cols = [] -%}
-{%- for col_name in  source_col_names -%}
-    {%- if col_name in  other_source_col_names -%}
-        {{ union_cols.append(col_name) or '' }}
-    {%- endif -%}
-{%- endfor -%}
+
+{# Get Unique Columns Across Both Datasets #}
+{%- set union_cols = source_col_names + other_source_col_names -%}
+{%- set union_cols = union_cols | unique | list -%}
+
 {# Generate Union Query #}
-SELECT {{ union_cols | join(', ') }} from {{ rasgo_source_ref(source_id) }}
+SELECT {{ '"' }}{{ union_cols | join('", "') }}{{ '"' }} FROM {{ rasgo_source_ref(source_id) }}
 UNION
-SELECT {{ union_cols | join(', ') }} from {{ source_table }}
+SELECT {{ '"' }}{{ union_cols | join('", "') }}{{ '"' }} FROM {{ source_table }}
