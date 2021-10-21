b4a4d2801e059b07f175ed1695e3b1d1a55e391d
Patrick <patrick@rasgoml.com>
2021-10-21 15:12:52 -0400

    fix pivot. it's really cool now.

--- Transforms/table_operations/rasgo_pivot.sql
+++ Transforms/table_operations/rasgo_pivot.sql
@@ -1,18 +1,16 @@
-{# args: {{dimensions}}, {{pivot_column}}, {{value_column}}, {{agg_method}}, {{list_of_vals}} #}
-
-{% set results, distinct_vals = None %}
-{%- set get_distinct_vals -%}
-    select distinct {{ value_column }}
-    from {{ source_table }}
-    limit 1000
+{%- set distinct_val_query -%}
+select distinct {{ value_column }}
+from {{ source_table }}
+limit 1000
 {%- endset -%}
 
-{%- if list_of_vals is defined -%}
-    {%- set distinct_vals = list_of_vals -%}
-{%- else -%}
-    {%- set results = run_query(get_distinct_vals) -%}
+{%- if list_of_vals is not defined -%}
+    {%- set results = run_query(distinct_val_query) -%}
     {%- set distinct_vals = results[results.columns[0]].to_list() -%}
+{%- else -%}
+    {%- set distinct_vals = list_of_vals -%}
 {%- endif -%}
+
 
 SELECT {{ dimensions }}, {{ distinct_vals | join(", ") }}
 FROM ( SELECT {{ dimensions }}, {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
