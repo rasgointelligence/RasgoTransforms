1dacddd7d7fc0bf7ca8e52e98fdb0271cd7bdf0c
Brian Stempin <bstempin@rasgoml.com>
2021-10-22 15:23:13 -0400

    Fixing the way I calculate the min and max for the EWB transformation

--- column_operations/rasgo_equal_width_binning/rasgo_equal_width_binning.sql
+++ column_operations/rasgo_equal_width_binning/rasgo_equal_width_binning.sql
@@ -1,5 +1,8 @@
 -- Args: {{bin_count}}, {{col_to_bin}}
 
 SELECT *,
-  width_bucket({{col_to_bin}}, MIN({{col_to_bin}}), MAX({{col_to_bin}}), {{bin_count}}) AS {{col_to_bin}}_{{bin_count}}_EWB
+  width_bucket({{col_to_bin}},
+      (SELECT MIN({{col_to_bin}}) FROM {{source_table}}),
+      (SELECT MAX({{col_to_bin}}) FROM {{source_table}}),
+      {{bin_count}}) AS {{col_to_bin}}_{{bin_count}}_EWB
 FROM {{ source_table }}
