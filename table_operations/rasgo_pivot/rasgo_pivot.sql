40a7cc1d73af99a0bdf5d2e6f6418299e6155149
Patrick <patrick@rasgoml.com>
2021-10-22 11:28:50 -0400

    fix table structure that i broke

--- table_operations/rasgo_pivot/rasgo_pivot.sql
+++ table_operations/rasgo_pivot/rasgo_pivot.sql
@@ -1,13 +1,17 @@
-{# args: {{dimensions}}, {{pivot_column}}, {{value_column}}, {{agg_method}} #}
-
-{%- set get_distinct_vals -%}
-    select distinct {{ value_column }}
-    from {{ source_table }}
-    limit 1000
+{%- set distinct_val_query -%}
+select distinct {{ value_column }}
+from {{ source_table }}
+limit 1000
 {%- endset -%}
-{%- set results = run_query(get_distinct_vals) -%}
-{%- set distinct_vals = results[results.columns[0]].to_list() -%}
 
-SELECT {{ dimensions }}, {{ distinct_vals | join(", ") }}
-FROM ( SELECT {{ dimensions }}, {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
-PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{ dimensions }}, {{ distinct_vals | join(", ") }} )
+{%- if list_of_vals is not defined -%}
+    {%- set results = run_query(distinct_val_query) -%}
+    {%- set distinct_vals = results[results.columns[0]].to_list() -%}
+{%- else -%}
+    {%- set distinct_vals = list_of_vals -%}
+{%- endif -%}
+
+
+SELECT {{ dimensions | join(", ") }}, {{ distinct_vals | join(", ") }}
+FROM ( SELECT {{ dimensions | join(", ") }}, {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
+PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{ dimensions | join(", ") }}, {{ distinct_vals | join(", ") }} )
