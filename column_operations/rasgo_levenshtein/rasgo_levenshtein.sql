668964c1b6e8bb86480c1b388c232d03ef35fbed
Alex Wolf <alex.r.wolf22@gmail.com>
2021-10-25 11:00:16 -0500

    added yaml for levenshtien distance

--- column_operations/rasgo_levenshtein/rasgo_levenshtein.sql
+++ column_operations/rasgo_levenshtein/rasgo_levenshtein.sql
@@ -1,11 +1,6 @@
--- args: {{Columns}}
-
--- should probably replace this batch slicer with a macro that creates all possible pairwise combinations from 'Columns'
--- this would be something we would use on any pairwise comparison functions
-
 SELECT *,
-{%- for ColumnPair in Columns|batch(2)|list %}
-    {%- for cols in ColumnPair -%}
+{%- for column_pair in columns | batch(2) | list %}
+    {%- for cols in column_pair -%}
         EDITDISTANCE({{cols[0]}}, {{cols[1]}}) as {{cols[0]}}_{{cols[1]}}_Distance {{ ", " if not loop.last else "" }}
     {%- endfor -%}
 {%- endfor -%}
