f072e8e36ced35f1894a72ce0ec29e6389906d15
Brian Stempin <bstempin@rasgoml.com>
2021-10-25 19:12:45 -0400

    Adding a join between the source table and the datespine table

--- table_operations/rasgo_datespine/rasgo_datespine.sql
+++ table_operations/rasgo_datespine/rasgo_datespine.sql
@@ -1,7 +1,23 @@
-select
-  dateadd(
-    '{{ interval_type }}',
-      {{ interval_amount}} * row_number() over (order by null),
-      {{ start_timestamp }}::timestamp_ntz)
-  ) as ts_ntz
-from table (generator(rowcount => {{ count }}));
+with date_spine as (
+    select
+           row_number() over (order by null) as interval_id
+           row_number(),
+           dateadd(
+               '{{ interval_type }}',
+               {{ interval_amount}} * row_number() over (order by null),
+               {{ start_timestamp }}::timestamp_ntz)) as ts_ntz_interval_start,
+            dateadd(
+               '{{ interval_type }}',
+               1 + ({{ interval_amount}} * row_number() over (order by null)),
+               {{ start_timestamp }}::timestamp_ntz)) as ts_ntz_interval_end
+from table (generator(rowcount => {{ count }}))
+    )
+select  {{ source_table }}.*,
+  date_spine.interval_id as {{ date_col }}_interval_id,
+  date_spine.ts_ntz_interval_start as {{ date_col }}_ts_ntz_interval_start,
+  date_spine.ts_ntz_interval_end as {{ date_col }}_ts_ntz_interval_end
+from {{ source_table }}
+join date_spine on
+    {{ source_table }}.{{ date_col }} >= date_spine.ts_ntz_interval_start
+    and
+    {{ source_table }}.{{ date_col }} < date_spine.ts_ntz_interval_end ;
