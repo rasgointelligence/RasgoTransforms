eff01c6d42d8ba80f31f25be5dbf6358e61f23ee
Alex Wolf <alex.r.wolf22@gmail.com>
2021-10-25 18:01:47 -0500

    updated yaml

--- table_operations/rasgo_unpivot/rago_unpivot.sql
+++ table_operations/rasgo_unpivot/rago_unpivot.sql
@@ -1,4 +1,2 @@
-{#- args: {{ value_column} }, {{ name_column }} {{ column_list_vals }} -#}
-
 SELECT * FROM {{ source_table }}
 UNPIVOT( {{ value_column }} for {{ name_column }} in ( {{ '"' }}{{ column_list_vals | join('", "')}}{{ '"' }} ))
