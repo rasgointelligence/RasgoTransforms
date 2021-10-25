52cb5e81ed891eb478c66c1604054e137bcb61a0
Brian Stempin <bstempin@rasgoml.com>
2021-10-25 19:29:36 -0400

    Syntax fixes from testing

--- table_operations/rasgo_datespine/rasgo_datespine.sql
+++ table_operations/rasgo_datespine/rasgo_datespine.sql
@@ -1,15 +1,14 @@
 with date_spine as (
     select
-           row_number() over (order by null) as interval_id
-           row_number(),
+           row_number() over (order by null) as interval_id,
            dateadd(
                '{{ interval_type }}',
                {{ interval_amount}} * row_number() over (order by null),
-               {{ start_timestamp }}::timestamp_ntz)) as ts_ntz_interval_start,
+               '{{ start_timestamp }}'::timestamp_ntz) as ts_ntz_interval_start,
             dateadd(
                '{{ interval_type }}',
                1 + ({{ interval_amount}} * row_number() over (order by null)),
-               {{ start_timestamp }}::timestamp_ntz)) as ts_ntz_interval_end
+               '{{ start_timestamp }}'::timestamp_ntz) as ts_ntz_interval_end
 from table (generator(rowcount => {{ count }}))
     )
 select  {{ source_table }}.*,
@@ -20,4 +19,4 @@ from {{ source_table }}
 join date_spine on
     {{ source_table }}.{{ date_col }} >= date_spine.ts_ntz_interval_start
     and
-    {{ source_table }}.{{ date_col }} < date_spine.ts_ntz_interval_end ;
+    {{ source_table }}.{{ date_col }} < date_spine.ts_ntz_interval_end
