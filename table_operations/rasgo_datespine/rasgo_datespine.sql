78d00a46ce93ddea1333fbc0a451b0cb470a046b
Brian Stempin <bstempin@rasgoml.com>
2021-10-26 12:25:13 -0400

    Updating documentation; updating SQL to generate intervals by date-part only

--- table_operations/rasgo_datespine/rasgo_datespine.sql
+++ table_operations/rasgo_datespine/rasgo_datespine.sql
@@ -1,15 +1,21 @@
+{% set row_count_query %}
+select datediff({{ interval_type }}, '{{ start_timestamp }}'::timestamp_ntz, '{{ end_timestamp }}'::timestamp_ntz)
+{% endset %}
+{% set row_count_query_results = run_query(row_count_query) %}
+{% set row_count = row_count_query_results[row_count_query_results.columns[0]][0] %}
+
 with date_spine as (
     select
            row_number() over (order by null) as interval_id,
            dateadd(
                '{{ interval_type }}',
-               {{ interval_amount}} * (interval_id - 1),
+               interval_id - 1,
                '{{ start_timestamp }}'::timestamp_ntz) as ts_ntz_interval_start,
             dateadd(
                '{{ interval_type }}',
-               {{ interval_amount}} * interval_id,
+               interval_id,
                '{{ start_timestamp }}'::timestamp_ntz) as ts_ntz_interval_end
-from table (generator(rowcount => {{ count }}))
+from table (generator(rowcount => {{ row_count }}))
     )
 select  {{ source_table }}.*,
   date_spine.interval_id as {{ date_col }}_interval_id,
