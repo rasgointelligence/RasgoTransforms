1a9240908a883e514f7e9b1af7fb347908f15e8b
Brian Stempin <bstempin@rasgoml.com>
2021-10-27 10:08:39 -0400

    Changing spacing so the output query is more readable

--- column_operations/rasgo_datepart/rasgo_datepart.sql
+++ column_operations/rasgo_datepart/rasgo_datepart.sql
@@ -1,5 +1,3 @@
-SELECT * ,
-{%- for col in date_columns -%}
-    DATE_PART({{date_part}}, "{{col}}") as {{col}}_datepart_{{date_part}} {{ ", " if not loop.last else "" }}
-{%- endfor -%}
+SELECT *,
+{% for col in date_columns %}DATE_PART({{date_part}}, "{{col}}") as {{col}}_datepart_{{date_part}} {{ ", " if not loop.last else "" }}{% endfor %}
 from {{ source_table }}
