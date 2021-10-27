f95633f1985e68b05d6864fdbcc97f477f7cc20e
Brian Stempin <bstempin@rasgoml.com>
2021-10-27 11:11:49 -0400

    Removing the ability to pass in a string col name; updating query formatting

--- column_operations/rasgo_todate/rasgo_todate.sql
+++ column_operations/rasgo_todate/rasgo_todate.sql
@@ -1,12 +1,3 @@
-{% set date_list = None %}
-{%- if date_column is string -%}
-    {% set date_list = [date_column] %}
-{% else %}
-    {% set date_list = date_column %}
-{%- endif -%}
-
-SELECT *, 
-{%- for col in date_list -%}
-    TO_DATE({{col}}, '{{format_expression}}') as {{col}}_todate {{ ", " if not loop.last else "" }}
-{%- endfor -%}
+SELECT *,
+{% for col in date_columns %}TO_DATE({{col}}, '{{format_expression}}') as {{col}}_todate {{ ", " if not loop.last else "" }}{% endfor %}
 from {{ source_table }}
