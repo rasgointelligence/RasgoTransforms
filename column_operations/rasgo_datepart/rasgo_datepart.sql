07839c96baeb99b1cb0b2fa5f4bdd43a56c9b65a
Brian Stempin <bstempin@rasgoml.com>
2021-10-27 09:56:16 -0400

    Removing the logic to accept both a string or a list as per PR review comments

--- column_operations/rasgo_datepart/rasgo_datepart.sql
+++ column_operations/rasgo_datepart/rasgo_datepart.sql
@@ -1,12 +1,5 @@
-{% set date_list = None %}
-{%- if date_column is string -%}
-    {% set date_list = [date_column] %}
-{% else %}
-    {% set date_list = date_column %}
-{%- endif -%}
-
-SELECT *, 
-{%- for col in date_list -%}
+SELECT * ,
+{%- for col in date_columns -%}
     DATE_PART({{date_part}}, {{col}}) as {{col}}_datepart_{{date_part}} {{ ", " if not loop.last else "" }}
 {%- endfor -%}
 from {{ source_table }}
