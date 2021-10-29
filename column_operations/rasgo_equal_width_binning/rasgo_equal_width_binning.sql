bcc429f846e26cfccfba8df6a2220bf06603c2f3
Brian Stempin <bstempin@rasgoml.com>
2021-10-29 12:27:06 -0400

    Getting rid of the old binning code

--- column_operations/rasgo_equal_width_binning/rasgo_equal_width_binning.sql
+++
@@ -1,6 +0,0 @@
-SELECT *,
-  width_bucket({{col_to_bin}},
-      (SELECT MIN({{col_to_bin}}) FROM {{source_table}}),
-      (SELECT MAX({{col_to_bin}}) FROM {{source_table}}),
-      {{bin_count}}) AS {{col_to_bin}}_{{bin_count}}_EWB
-FROM {{ source_table }}
--- column_operations/rasgo_n_tile_binning/rasgo_n_tile_binning.sql
+++
@@ -1,3 +0,0 @@
-SELECT *,
-  ntile({{bin_count}}) OVER (ORDER BY {{col_to_bin}}) AS {{col_to_bin}}_{{bin_count}}_NTB
-FROM {{ source_table }}
