8ca974fbc114a802cc8f1d9990d8ff8e07cb9005
Alex Wolf <alex.r.wolf22@gmail.com>
2021-11-02 14:25:12 -0500

    fixed github url

--- docs/docs_generation/doc_generator.py
+++ docs/docs_generation/doc_generator.py
@@ -18,8 +18,8 @@ def save_transform_docs() -> None:
     transform_yamls = utils.load_all_yaml_files()
     for transform_type, transform_type_yamls in transform_yamls.items():
         for transform_name, transform_data in transform_type_yamls.items():
-            transform_name = transform_name.replace("_", "-")
-            print(f"Generating Markdown for Transform '{transform_type}/{transform_name}.yaml'")
+            transform_name_dash = transform_name.replace("_", "-")
+            print(f"Generating Markdown for Transform '{transform_type}/{transform_name_dash}.yaml'")
             markdown = _get_transform_markdown(
                 transform_data=transform_data,
                 transform_type=transform_type,
