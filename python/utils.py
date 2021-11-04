959d7bd64c9c528997606c5eb77d508f563a8cba
Alex Wolf <alex.r.wolf22@gmail.com>
2021-11-04 17:12:37 -0500

    updates

--- python/utils.py
+++ python/utils.py
@@ -1,14 +1,20 @@
 """
 Utils for Documentation Generation from UDT Yaml Files
 """
+import os
 import subprocess
 from collections import defaultdict
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Set
 
 import yaml
+from pyrasgo.rasgo import Rasgo
 
 from . import constants
+
+# ----------------------------------------------
+#      Utils for Doc and Transform Creation
+# ----------------------------------------------
 
 
 def load_all_yaml_files() -> Dict[str, Dict[str, Dict]]:
@@ -39,6 +45,72 @@ def load_all_yaml_files() -> Dict[str, Dict[str, Dict]]:
     return transform_yamls
 
 
+# ----------------------------------------------
+#          Utils for Transform Creation
+# ----------------------------------------------
+
+
+def set_rasgo_domain_env(rasgo_domain: str) -> None:
+    """
+    From Rasgo Domain/Enum Name, return the rasgo url for it
+
+    :param rasgo_domain: 'local', 'staging', or 'production
+    """
+    rasgo_domains = constants.PyRasgoEnvironment._member_map_
+    rasgo_domain_url = rasgo_domains[rasgo_domain.upper()].value
+    os.environ["RASGO_DOMAIN"] = rasgo_domain_url
+
+
+def get_all_rasgo_transform_names(rasgo: Rasgo) -> Set[str]:
+    """
+    Return a list of all transforms names available to
+    the logged in Rasgo User
+    """
+    transform_in_rasgo = rasgo.get.transforms()
+    return {t.name for t in transform_in_rasgo}
+
+
+def get_transform_source_code(transform_type: str, transform_name: str) -> str:
+    """
+    From a transform name and type load and return it's source code as a string
+    """
+    root_dir = get_root_dir()
+    source_code_path = root_dir / transform_type / transform_name / f"{transform_name}.sql"
+    fp = open(source_code_path)
+    source_code = fp.read()
+    fp.close()
+    return source_code
+
+
+def parse_transform_args_from_yaml(transform_yaml: Dict) -> List[Dict[str, str]]:
+    """
+    From a loaded Transform Yaml File parse the
+    Transform args in proper format, return the args
+    in proper format for transform creation in PyRasgo
+    """
+    transform_args = []
+    for arg_name, arg_meta_data in transform_yaml['arguments'].items():
+        transform_args.append(
+            {**{'name': arg_name}, **arg_meta_data}
+        )
+    return transform_args
+
+
+def get_root_dir() -> Path:
+    """
+    Get and return the root directory absolute path of this git repo
+    """
+    cmd = ["git", "rev-parse", "--show-toplevel"]
+    root_dir_bytes = subprocess.check_output(cmd)
+    root_dir_str = root_dir_bytes.decode('utf-8').strip()
+    return Path(root_dir_str)
+
+
+# ----------------------------------------------
+#          Utils for Docs Generation
+# ----------------------------------------------
+
+
 def get_table_values(transform_args: Dict) -> List[List[str]]:
     """
     From a Transform Args Dict derived from YML file,
@@ -56,14 +128,9 @@ def get_table_values(transform_args: Dict) -> List[List[str]]:
     return all_data
 
 
-def get_root_dir() -> Path:
-    """
-    Get and return the root directory absolute path of this git repo
-    """
-    cmd = ["git", "rev-parse", "--show-toplevel"]
-    root_dir_bytes = subprocess.check_output(cmd)
-    root_dir_str = root_dir_bytes.decode('utf-8').strip()
-    return Path(root_dir_str)
+# ----------------------------------------------
+#      Private Helper Funcs for this File
+# ----------------------------------------------
 
 
 def _read_yaml(yaml_path: Path) -> Dict:
