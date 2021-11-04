1f24929e42e8f164e1b50ab36e547e63028566e3
Patrick <patrick@rasgoml.com>
2021-11-03 23:58:35 -0400

    fix some syntax

--- column_transforms/rasgo_lagagg/rasgo_lagagg.sql
+++ column_transforms/rasgo_lagagg/rasgo_lagagg.sql
@@ -1,11 +1,11 @@
 SELECT *, 
     {% for col in columns %}
         {%- for window in windows -%}
-            {{ AGG_TYPE }}({CASE WHEN {{DATE}} - {{EVENT_DATE}} > 0 and {{DATE}} - {{EVENT_DATE}} < {{window}} THEN {{col}}) as {{col}}_{{AGG_TYPE}}_{{window}}{{ ", " if not loop.last else "" }}
+            {{ agg_type }}(CASE WHEN {{date}} - {{event_date}} > 0 and {{date}} - {{event_date}} < {{window}} THEN {{col}}) as {{col}}_{{agg_type}}_{{window}}{{ ", " if not loop.last else "" }}
         {%- endfor -%}
         {{ ", " if not loop.last else "" }}
     {% endfor %}
 from {{ source_table }} a
 left join
-rasgo_source_ref({{EVENT_DATASET}}) b
+rasgo_source_ref({{event_dataset}}) b
 on a.ID = b.ID
