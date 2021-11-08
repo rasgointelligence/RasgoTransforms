292ba2f31c55114c4ab8ec1434d7cc8cbd56bbb4
Alex Wolf <alex.r.wolf22@gmail.com>
2021-11-08 13:42:39 -0600

    date diff accept regualr time too

--- column_transforms/rasgo_datediff/rasgo_datediff.sql
+++ column_transforms/rasgo_datediff/rasgo_datediff.sql
@@ -1,3 +1,3 @@
 SELECT *,
-DATEDIFF({{ date_part }}, {{ date_col_1 }}, {{ date_col_2 }}) as {{ date_col_2 }}_{{ date_col_1 }}_{{ date_part }}_datediff
+DATEDIFF({{ date_part }}, {{ date_val_1 }}, {{ date_val_2 }}) as cleanse_name(date_val_2 + '_' + date_val_1  + '_' + date_part + '_datediff')
 FROM {{ source_table }}
