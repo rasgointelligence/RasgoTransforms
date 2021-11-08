e447bb0248702e96c727c9d37d25222532cddc41
Patrick <patrick@rasgoml.com>
2021-11-08 11:40:30 -0500

    change name of date_dim

--- column_transforms/rasgo_movingavg/rasgo_movingavg.sql
+++ column_transforms/rasgo_movingavg/rasgo_movingavg.sql
@@ -1,7 +1,7 @@
 SELECT * 
 {%- for column in input_columns -%}
     {%- for window in window_sizes -%}
-        , avg({{column}}) OVER(PARTITION BY {{partition | join(", ")}} ORDER BY {{date_dim}} ROWS BETWEEN {{window - 1}} PRECEDING AND CURRENT ROW) AS mean_{{column}}_{{window}} 
+        , avg({{column}}) OVER(PARTITION BY {{partition | join(", ")}} ORDER BY {{order_by}} ROWS BETWEEN {{window - 1}} PRECEDING AND CURRENT ROW) AS mean_{{column}}_{{window}} 
     {%- endfor -%}
 {%- endfor -%}
 FROM {{ source_table }}
