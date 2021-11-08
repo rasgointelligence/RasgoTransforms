764b2f3f5f08f141d855d0b2a77046248125c76e
Patrick <patrick@rasgoml.com>
2021-11-08 18:37:49 -0500

    make the fixes

--- column_transforms/rasgo_datetrunc/rasgo_datetrunc.sql
+++ column_transforms/rasgo_datetrunc/rasgo_datetrunc.sql
@@ -1,12 +1,12 @@
 {% set date_list = None %}
-{%- if date_column is string -%}
-    {% set date_list = [date_column] %}
+{%- if date_columns is string -%}
+    {% set date_list = [date_columns] %}
 {% else %}
-    {% set date_list = date_column %}
+    {% set date_list = date_columns %}
 {%- endif -%}
 
 SELECT *, 
 {%- for col in date_list -%}
-    DATE_TRUNC({{date_part}}, {{col}}) as {{col}}_{{date_part}} {{ ", " if not loop.last else "" }}
+    DATE_TRUNC('{{date_part}}', {{col}}) as {{col}}_{{date_part}} {{ ", " if not loop.last else "" }}
 {%- endfor -%}
 from {{ source_table }}
