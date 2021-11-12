2897d000efc023c6b12b6412c84fedede2d568bf
SpencerZaghikianRasgoXPS15 <szaghikian@rasgoml.com>
2021-11-12 16:19:50 -0500

    cleanse names and describe functionality of dedupe

--- column_transforms/rasgo_cast/rasgo_cast.sql
+++ column_transforms/rasgo_cast/rasgo_cast.sql
@@ -4,7 +4,7 @@ Rasgo Cast Error: The Column list must be the same length as the Type list.
 SELECT
 *
 {%- for target_col in col_list %}
-    , TRY_CAST({{target_col}} AS {{type_list[loop.index-1]}})
+    , TRY_CAST({{target_col}} AS {{type_list[loop.index-1]}}) AS {{cleanse_name(target_col)+'_'+cleanse_name(type_list[loop.index-1])}}
 {%- endfor %}
 FROM {{ source_table }}
 
--- column_transforms/rasgo_concat/rasgo_concat.sql
+++ column_transforms/rasgo_concat/rasgo_concat.sql
@@ -4,5 +4,5 @@ SELECT
     {%- for obj in concat_list %}
     {{obj}}{{ ", " if not loop.last else "" }}
     {%- endfor %}
-)
+) AS CONCAT_{%- for obj in concat_list %}{{cleanse_name(obj)}}{%- endfor %}
 FROM {{ source_table }}
