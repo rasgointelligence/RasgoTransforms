e00f799df691a147359329cee6fbcbfb48c484e9
Patrick <patrick@rasgoml.com>
2021-11-15 14:46:16 -0500

    add cleanse_name to fix errors

--- table_transforms/rasgo_pivot/rasgo_pivot.sql
+++ table_transforms/rasgo_pivot/rasgo_pivot.sql
@@ -14,4 +14,8 @@ limit 1000
 
 SELECT {{ dimensions | join(", ") }}, {{ distinct_vals | join(", ") }}
 FROM ( SELECT {{ dimensions | join(", ") }}, {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
-PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p ( {{ dimensions | join(", ") }}, {{ distinct_vals | join(", ") }} )
+PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p
+( {{ dimensions | join(", ") }}, 
+{%- for val in distinct_vals -%}
+{{ cleanse_name(val) }}{{ ', ' if not loop.last else '' }}
+{%- endfor -%} )
