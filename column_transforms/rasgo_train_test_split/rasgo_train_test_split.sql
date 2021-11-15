ee7b2d9ce1ba6a2d72f7bdabd78584e2e7a86e3c
Patrick <patrick@rasgoml.com>
2021-11-14 19:35:58 -0500

    add support for random sampling

--- column_transforms/rasgo_train_test_split/rasgo_train_test_split.sql
+++ column_transforms/rasgo_train_test_split/rasgo_train_test_split.sql
@@ -1,4 +1,7 @@
 SELECT *,
-CASE WHEN ROW_NUMBER() OVER (ORDER BY {{order_by | join(", ")}} ) < (COUNT(1) OVER () * {{train_percent}}) THEN 'Train' ELSE 'Test' END AS TT_SPLIT
-
+{%- if order_by is defined %}
+CASE WHEN ROW_NUMBER() OVER (ORDER BY {{order_by | join(", ")}} ) < (COUNT(1) OVER () * {{train_percent}}) THEN 'TRAIN' ELSE 'TEST' END AS TT_SPLIT
+{%- else %}
+CASE WHEN MOD(RANDOM(),  1/{{train_percent}}) = 0 THEN 'TRAIN' ELSE 'TEST' END AS TT_SPLIT
+{%- endif %}
 FROM {{ source_table }}
