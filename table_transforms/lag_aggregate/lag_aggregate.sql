7a297420b3fd0068618090c57fcfce03a3f66565
Miroslav Maraz <miro@untitled-research.com>
2021-12-07 16:36:31 +0000

    initial lag_aggregate implementation with example & notes

---
+++ table_transforms/lag_aggregate/lag_aggregate.sql
@@ -0,0 +1,29 @@
+SELECT
+    {%- for s_col in source_join_columns %}
+        fct.{{ s_col }},
+    {%- endfor -%}
+    {%- for lag in lag_interval_list %}
+        {%- set outer_loop = loop -%}
+        {%- for col, aggs in aggregations.items() %}
+            {%- for agg in aggs %}
+                {{ agg }}(
+                    CASE
+                        WHEN 
+                            fct.{{ source_temporal_column}} >= (base.{{ base_temporal_column }} - INTERVAL '{{lag}} {{ lag_period_type}}')
+                            AND fct.{{ source_temporal_column}} < base.{{ base_temporal_column }}
+                        THEN {{ col }} 
+                    END) as {{ col + '_' + agg + '_' }}{{ lag }}{{ '' if loop.last and outer_loop.last else ',' }}
+            {%- endfor -%}
+        {%- endfor %}
+    {%- endfor %}
+FROM {{ source_table }} fct
+JOIN {{ rasgo_source_ref(base_source_id) }} base
+{%- for s_col in source_join_columns %}
+    {{ ' AND' if not loop.first else 'ON'}} fct.{{ s_col }} = base.{{ base_join_columns[loop.index0] }}    
+{%- endfor -%}
+   -- for some strange reason you must leave a blank line here otherwise sql gets squished when rendered
+
+GROUP BY
+{%- for s_col in source_join_columns %}
+    fct.{{ s_col }}{{ '' if loop.last else ',' }}
+{%- endfor -%}
