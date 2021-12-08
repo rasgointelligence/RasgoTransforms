7a82e6a575d7c8590782163e9bdcfdb7499f67c3
SpencerZaghikianRasgoXPS15 <szaghikian@rasgoml.com>
2021-12-08 10:38:10 -0500

    clean up docs

--- column_transforms/datepart/datepart.sql
+++ column_transforms/datepart/datepart.sql
@@ -1,4 +1,3 @@
 SELECT *,
-// TODO: at odds w docs
 {% for col in date_columns %}DATE_PART('{{date_part}}', {{col}}) as {{col}}_datepart_{{date_part}} {{ ", " if not loop.last else "" }}{% endfor %}
 from {{ source_table }}
