3426e1547ab1334c29714254d77694b8c4a5eece
Patrick <patrick@rasgoml.com>
2021-12-09 21:50:35 -0500

    new transform from miro

---
+++ column_transforms/lag_aggregate/lag_aggregate.sql
@@ -0,0 +1,29 @@
+SELECT
+    {%- for s_col in source_join_columns %}
+        fct.{{ s_col }},
+    {%- endfor -%}
+    fct.{{ source_temporal_column }},
+    {%- for lag in lag_interval_list %}
+        {%- set outer_loop = loop -%}
+        {%- for col, aggs in aggregations.items() %}
+            {%- for agg in aggs %}
+                {{ agg }}(
+                    CASE
+                        WHEN 
+                            fct.{{ source_temporal_column}} >= (base.{{ base_temporal_column }} - INTERVAL '{{lag}} {{ lag_period_type}}')
+                            AND fct.{{ source_temporal_column}} < base.{{ base_temporal_column }}
+                        THEN {{ col }} 
+                    END) as {{ col + '_' + agg + '_' }}{{ lag }}{{ '' if loop.last and outer_loop.last else ',' }}
+            {%- endfor -%}
+        {%- endfor %}
+    {%- endfor %}
+FROM {{ source_table }} fct
+JOIN {{ rasgo_source_ref(base_source_id) }} base
+{%- for s_col in source_join_columns %}
+    {{ ' AND' if not loop.first else 'ON'}} fct.{{ s_col }} = base.{{ base_join_columns[loop.index0] }}    
+{%- endfor %}
+GROUP BY
+fct.{{ source_temporal_column }},
+{%- for s_col in source_join_columns %}
+    fct.{{ s_col }}{{ '' if loop.last else ',' }}
+{%- endfor -%}
