b8c71ea92a55ab9b2dc9e927ca37a9708af28243
Patrick <patrick@rasgoml.com>
2021-12-14 21:15:14 -0500

    Update multi_join.sql

--- table_transforms/multi_join/multi_join.sql
+++ table_transforms/multi_join/multi_join.sql
@@ -6,22 +6,19 @@
 
 {# Jinja Macro to get columns for a table #}
 {%- macro get_column_names(fqtn=None) -%}
-    {%- set database, schema, table = '', '', '' -%}
-    {%- if source_table_fqtn -%}
-        {%- set database, schema, table = source_table_fqtn.split('.') -%}
-    {%- endif -%}
-        SELECT COLUMN_NAME FROM {{ database }}.information_schema.columns
-        WHERE TABLE_CATALOG = '{{ database|upper }}'
-        AND   TABLE_SCHEMA = '{{ schema|upper }}'
-        AND   TABLE_NAME = '{{ table|upper }}'
+    {%- set database, schema, table = source_table_fqtn.split('.') -%}
+    SELECT COLUMN_NAME FROM {{ database }}.information_schema.columns
+    WHERE TABLE_CATALOG = '{{ database|upper }}'
+    AND   TABLE_SCHEMA = '{{ schema|upper }}'
+    AND   TABLE_NAME = '{{ table|upper }}'
 {%- endmacro -%}
 
 SELECT
 t0.*
-{% for table in join_tables -%}
-{% set o_loop = loop %}
-{%- set table_only = get_table_name(fqtn=table) -%}
-{%- set col_names_df = run_query(get_column_names(fqtn=table)) -%}
+{% for fqtn in join_tables -%}
+{%- set o_loop = loop -%}
+{%- set table_only = get_table_name(fqtn=fqtn) -%}
+{%- set col_names_df = run_query(get_column_names(fqtn=fqtn)) -%}
 {%- set col_names = col_names_df['COLUMN_NAME'].to_list() -%}
     {% for column in col_names -%}
 , t{{o_loop.index}}.{{column}} as {{table_only}}_{{column}}
@@ -30,10 +27,10 @@ t0.*
 
 FROM
 {{ source_table }} t0
-{% for table in join_tables -%}
+{% for fqtn in join_tables -%}
 {% set outer_loop = loop %}
 {{ join_type + ' ' | upper }}JOIN
-{{ table }} t{{loop.index}}
+{{ fqtn }} t{{loop.index}}
     {%- for join_col in join_columns -%}
     {{' AND' if not loop.first else 'ON'}} t0.{{ join_col }} = t{{ outer_loop.index }}.{{ join_col }}
     {% endfor -%}
