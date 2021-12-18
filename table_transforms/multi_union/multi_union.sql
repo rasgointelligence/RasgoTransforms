5b70c57d6b65c90cc675d15d6c4055c0e6895e5e
Patrick <patrick@rasgoml.com>
2021-12-18 11:13:18 -0500

    Update multi_union.sql

--- table_transforms/multi_union/multi_union.sql
+++ table_transforms/multi_union/multi_union.sql
@@ -6,13 +6,13 @@
 
 {%- set table_only = get_table_name(fqtn=source_table) -%}
 
-SELECT table_only as origin_table, {{union_columns | join(", ")}}
+SELECT '{{table_only}}' as origin_table, {{union_columns | join(", ")}}
 FROM {{ source_table }}
 
 {% for fqtn in union_tables -%}
 {%- set table_name = get_table_name(fqtn=fqtn) -%}
 UNION ALL
 
-SELECT table_name as origin_table, {{union_columns | join(", ")}}
+SELECT '{{table_name}}' as origin_table, {{union_columns | join(", ")}}
 FROM {{ fqtn }}
 {% endfor %}
