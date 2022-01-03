424f88a1670ed9c6eb5296e535481b499f757daa
Patrick <patrick@rasgoml.com>
2022-01-03 13:42:46 -0500

    fix datepart

--- column_transforms/datepart/datepart.sql
+++ column_transforms/datepart/datepart.sql
@@ -1,5 +1,5 @@
 SELECT *,
 {%- for target_col, date_part in dates.items() %}
-    DATE({{target_col}}, '{{date_part}}') as {{target_col}}_date {{ ", " if not loop.last else "" }}
+    DATE_PART('{{date_part}}', {{target_col}}) as {{target_col}}_{{date_part}} {{ ", " if not loop.last else "" }}
 {%- endfor %}
 from {{ source_table }}
