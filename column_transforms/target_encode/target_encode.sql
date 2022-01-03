5edecc2699d1943bfe0e273d1be3f2eb524c51dc
Patrick <patrick@rasgoml.com>
2022-01-03 13:44:17 -0500

    Fix some transforms (#74)
    * Update target_encode.sql
    
    * Update lag.sql
    
    * fix datepart

--- column_transforms/target_encode/target_encode.sql
+++ column_transforms/target_encode/target_encode.sql
@@ -1,9 +1,9 @@
 with means as (
-    select distinct {{column}} as value, ROUND(AVG({{target}}), 3) as mean_encode
+    select distinct {{column}} as value, ROUND(AVG({{target}}), 3) as {{column}}_target_encoded
     from {{ source_table }}
     group by value)
 
-select t.*, m.mean_encode
+select t.*, m.{{column}}_target_encoded
 from {{ source_table }} t
 left join
 means m
