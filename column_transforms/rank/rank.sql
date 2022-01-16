f6e980ec80ff8dc26bcac141097f31f08ab1d1d3
griffatrasgo <70294474+griffatrasgo@users.noreply.github.com>
2022-01-16 12:05:45 -0500

    first try

---
+++ column_transforms/rank/rank.sql
@@ -0,0 +1,25 @@
+SELECT
+*
+{% if rank_type = 'dense' %}
+  , DENSE_RANK() OVER(
+{% elif rank_type = 'percent' %}
+  , PERCENT_RANK() OVER(
+{% elif rank_type = 'unique' %}
+  , ROW_NUMBER() OVER(
+{% else %}
+  , RANK() OVER(
+{% endif %}
+{% if partition_by %}
+PARTITON BY partition_by
+{% endif %}
+ORDER BY rank_columns
+{% if order %}
+ order
+{% endif %}
+) AS AS RANK_{{ cleanse_name(rank_columns) }}
+
+FROM {{ source_table }}
+
+{% if qualify_filter %}
+QUALIFY RANK_{{ cleanse_name(rank_columns) }} qualify_filter
+{% endif %}
