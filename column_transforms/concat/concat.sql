1ad624ce6463a40e38ef0b06fd6cfc86eb01193e
Patrick <patrick@rasgoml.com>
2022-01-30 20:44:43 -0500

    Support a couple of aliases (#95)
    * start with concat
    
    * Added Auto Generated Transform docs
    
    * math
    
    * Added Auto Generated Transform docs
    
    * Update math.yaml
    
    * Added Auto Generated Transform docs
    
    Co-authored-by: GitHub <noreply@github.com>

--- column_transforms/concat/concat.sql
+++ column_transforms/concat/concat.sql
@@ -1,8 +1,14 @@
+{%- if name is defined -%}
+    {%- set alias = cleanse_name(name) -%}
+{%- else -%}
+    {%- set alias = 'CONCAT_'~ cleanse_name(concat_list | join('_')) -%}
+{%- endif -%}
+
 SELECT
 *
 , CONCAT(
     {%- for obj in concat_list %}
     {{obj}}{{ ", " if not loop.last else "" }}
     {%- endfor %}
-) AS CONCAT_{%- for obj in concat_list %}{{cleanse_name(obj)}}{%- endfor %}
+) AS {{ alias }}
 FROM {{ source_table }}
