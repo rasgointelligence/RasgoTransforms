86886f05d89c0fae42952c1a4ed5660fdc80d76a
griffatrasgo <70294474+griffatrasgo@users.noreply.github.com>
2022-02-03 14:19:11 -0500

    fixes from testing

--- table_transforms/rolling_agg/rolling_agg.sql
+++ table_transforms/rolling_agg/rolling_agg.sql
@@ -1,18 +1,18 @@
 SELECT *
 {% for col, aggs in aggregations.items() -%}
   {%- for agg in aggs -%}
-    {%- for offset in offsets -%}
+    {%- for offset in offsets %}
       , {{ agg }}({{ col }}) OVER( 
         {%- if group_by %}
         PARTITION BY {{ group_by | join(", ") }} 
         {% endif -%}
-        ORDER BY {{order_by}}) 
-        {% if offset > 0 %}
-        ROWS BETWEEN CURRENT ROW AND {{ offset }} FOLLOWING
-        {% else %}
-        ROWS BETWEEN {{ offset }} PRECEDING AND CURRENT ROW
-        {% endif %}
-      ) as {{ cleanse_name(agg + '_' + col + + offset|string) }}
+        ORDER BY {{order_by}} 
+        {% if offset > 0 -%}
+        ROWS BETWEEN CURRENT ROW AND {{ offset|replace('-','') }} FOLLOWING
+        {% else -%}
+        ROWS BETWEEN {{ offset|replace('-','') }} PRECEDING AND CURRENT ROW
+        {% endif -%}
+      ) as {{ cleanse_name(agg + '_' + col + '_' + offset|string) }}
     {%- endfor -%}
   {%- endfor -%}
 {%- endfor %}
