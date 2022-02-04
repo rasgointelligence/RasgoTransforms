f216f58ecdbdcc066881ff8d37f9598a26f82711
Josh Berry <jberry@rasgoml.com>
2022-02-03 23:55:31 -0500

    fixed white spaces
    paid better attention to white space

--- table_transforms/aggregate/aggregate.sql
+++ table_transforms/aggregate/aggregate.sql
@@ -1,20 +1,18 @@
-SELECT
-{%- for group_item in group_by %}
-    {{ group_item }},
-{%- endfor -%}
-
-{%- for k, vals in aggregations.items() %}
-        {%- set outer_loop = loop -%}
-    {%- for v in vals %}
-        {% if agg_key=='metric' %}
-            {% set agg = k %}
-            {% set col = v %}
-        {% else %}
-            {% set agg = v %}
-            {% set col = k %}
-        {% endif %}
-    {{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
-    {%- endfor -%}
+SELECT {% for group_item in group_by %}
+{{ group_item }}, 
 {%- endfor %}
-FROM {{ source_table }}
-GROUP BY {{ group_by | join(', ') }}
+{%- for k, vals in aggregations.items() -%}
+   {%- set outer_loop = loop -%}
+      {%- for v in vals -%}
+      {%- if agg_key=='metric' -%}
+          {%- set agg = k -%}
+          {%- set col = v -%}
+      {%- else -%}
+          {%- set agg = v -%}
+          {%- set col = k -%}
+      {%- endif %}
+{{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
+    {%- endfor -%}
+{% endfor %}
+ FROM {{ source_table }}
+ GROUP BY {{ group_by | join(', ') }}
