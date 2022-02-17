c4816e83d6dc3c5019fa41c11682e2dbc1398bb0
griffatrasgo <70294474+griffatrasgo@users.noreply.github.com>
2022-02-17 17:35:51 -0500

    invert signs

--- rasgotransforms/rasgotransforms/table_transforms/rolling_agg/rolling_agg.sql
+++ rasgotransforms/rasgotransforms/table_transforms/rolling_agg/rolling_agg.sql
@@ -2,15 +2,16 @@ SELECT *
 {% for col, aggs in aggregations.items() -%}
   {%- for agg in aggs -%}
     {%- for offset in offsets %}
+      {% set normalized_offset = -offset %}
       , {{ agg }}({{ col }}) OVER( 
         {%- if group_by %}
         PARTITION BY {{ group_by | join(", ") }} 
         {% endif -%}
         ORDER BY {{ order_by | join(", ") }} 
-        {% if offset > 0 -%}
-        ROWS BETWEEN CURRENT ROW AND {{ offset|replace('-','') }} FOLLOWING
+        {% if offset < 0 -%}
+        ROWS BETWEEN CURRENT ROW AND {{ normalized_offset|abs }} FOLLOWING
         {% else -%}
-        ROWS BETWEEN {{ offset|replace('-','') }} PRECEDING AND CURRENT ROW
+        ROWS BETWEEN {{ normalized_offset|abs }} PRECEDING AND CURRENT ROW
         {% endif -%}
       ) as {{ cleanse_name(agg + '_' + col + '_' + offset|string) }}
     {%- endfor -%}
