22c088988e40f1f5d3c953b7063a2f6a02272d39
RasgoDerek <dchadwell@rasgoml.com>
2022-03-15 13:36:54 -0400

    allow no group by and add entropy calculations

--- rasgotransforms/rasgotransforms/table_transforms/aggregate/aggregate.sql
+++ rasgotransforms/rasgotransforms/table_transforms/aggregate/aggregate.sql
@@ -1,13 +1,40 @@
+{%- set entropy_aggs = {} -%}
+{%- set from_tables = [source_table] -%}
+{%- for col, aggs in aggregations.items() -%}
+    {%- if 'ENTROPY' in aggs -%}
+        {%- set _ = entropy_aggs.update({col: aggs}) -%}
+    {%- endif -%}
+{%- endfor -%}
+
+{%- for col, aggs in entropy_aggs.items() -%}
+    {%- if loop.first %}
+        with
+    {%- endif %}
+    {{ col }}_COUNTS as (
+        select {{col}}, count(*) as c from {{ source_table }} group by 1
+    ),
+    {{ col }}_RATIOS as (
+        select ratio_to_report(c) over() p_{{col}} from {{ col }}_COUNTS
+    ) {{ ',' if not loop.last }}
+    {%- set _ = from_tables.append(col+'_RATIOS')  -%}
+{%- endfor -%}
 SELECT
-{%- for group_item in group_by %}
+{% for group_item in group_by -%}
     {{ group_item }},
 {%- endfor -%}
 
-{%- for col, aggs in aggregations.items() %}
+{%- for col, aggs in aggregations.items() -%}
         {%- set outer_loop = loop -%}
-    {%- for agg in aggs %}
-    {{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
+    {%- for agg in aggs -%}
+        {%- if agg == 'ENTROPY' -%}
+            -sum(p_{{ col }}*log(2,p_{{ col }})) as {{ col }}_entropy
+        {%- else -%}
+            {{ agg }}({{ col }}) as {{ col + '_' + agg }}
+        {%- endif -%}
+        {{ '' if loop.last and outer_loop.last else ',' }}
     {%- endfor -%}
 {%- endfor %}
-FROM {{ source_table }}
-GROUP BY {{ group_by | join(', ') }}
+FROM {{ from_tables | join(', ') }}
+{%- if group_by -%}
+    GROUP BY {{ group_by | join(', ') }}
+{%- endif -%}
