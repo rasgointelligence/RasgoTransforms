5da6753c80900c4059e66e8cba8a5ade1d5f7477
Josh Berry <jberry@rasgoml.com>
2022-03-16 15:18:44 -0400

    added discretion for final select list to avoid repulling same keys over and over

--- rasgotransforms/rasgotransforms/table_transforms/timeseries_agg/snowflake/timeseries_agg.sql
+++ rasgotransforms/rasgotransforms/table_transforms/timeseries_agg/snowflake/timeseries_agg.sql
@@ -1,3 +1,4 @@
+{%- set final_col_list = [] -%}
 WITH
 {%- for offset in offsets -%}
   {% set normalized_offset = -offset %}
@@ -7,16 +8,18 @@ OFFSET{{ cleanse_name(offset|string + date_part) }} AS (
     A.{{ g }} AS OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ g }},
       {%- endfor %}   
   A.{{ date }} AS OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ date }},
-        {% for col, aggs in aggregations.items() -%}
-        {%- set inner_loop = loop -%}
-            {%- for agg in aggs %}
-            {%- if normalized_offset > 0 %}
-                {{ agg }}(B.{{ col }}) AS {{ cleanse_name(agg + '_' + col + '_NEXT' + offset|string + date_part) }} {{ '' if loop.last and inner_loop.last else ',' }}
-            {%- else %}
-                {{ agg }}(B.{{ col }}) AS {{ cleanse_name(agg + '_' + col + '_PAST' + offset|string + date_part) }} {{ '' if loop.last and inner_loop.last else ',' }}
-            {%- endif %}
-    {%- endfor -%}
-  {%- endfor %}
+{% for col, aggs in aggregations.items() -%}
+  {%- set inner_loop = loop -%}
+  {%- for agg in aggs %}
+    {% if normalized_offset > 0 -%}
+      {%- set alias = cleanse_name(agg ~ '_' ~ col ~ '_NEXT' + offset|string + date_part) %}
+    {%- else -%}
+      {%- set alias = cleanse_name(agg ~ '_' ~ col ~ '_NEXT' + offset|string + date_part) %}
+    {%- endif -%}
+    {{ agg }}(B.{{ col }}) AS {{ alias }}{{ '' if loop.last and inner_loop.last else ',' }}
+    {%- do final_col_list.append(alias) -%}
+  {%- endfor -%}
+{%- endfor %}
   FROM {{ source_table }} A
   INNER JOIN {{ source_table }} B
   ON {% for g in group_by -%}
@@ -33,13 +36,14 @@ OFFSET{{ cleanse_name(offset|string + date_part) }} AS (
   A.{{ g }}, {% endfor -%}
   A.{{ date }}) {{ '' if loop.last else ',' }}
 {% endfor -%}
-SELECT * FROM {{ source_table }} src
+SELECT src.*, 
+{{ final_col_list|join(', ') }} FROM {{ source_table }} src
 {% for offset in offsets -%}
   {% set normalized_offset = -offset %}
 LEFT OUTER JOIN OFFSET{{ cleanse_name(offset|string + date_part) }} 
 ON OFFSET{{ cleanse_name(offset|string + date_part) }}.OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ date }} = src.{{ date }}
       {%- for g in group_by %}
-        AND src.{{ g }} = OFFSET{{ cleanse_name(offset|string + date_part) }}.OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ g }}
-      {% endfor -%}
-      AND src.{{ date }} = OFFSET{{ cleanse_name(offset|string + date_part) }}.OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ date }} 
+  AND src.{{ g }} = OFFSET{{ cleanse_name(offset|string + date_part) }}.OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ g }}
+  {% endfor -%}
+  AND src.{{ date }} = OFFSET{{ cleanse_name(offset|string + date_part) }}.OFFSET{{ cleanse_name(offset|string + date_part) }}_{{ date }} 
 {%- endfor -%}
