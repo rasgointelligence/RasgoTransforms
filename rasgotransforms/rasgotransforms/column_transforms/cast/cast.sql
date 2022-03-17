6b67c0f2b4bad64d0cf74721b798d9388679ccdf
Patrick <patrick@rasgoml.com>
2022-03-17 16:57:12 -0400

    add overwrite_columns to cast

--- rasgotransforms/rasgotransforms/column_transforms/cast/cast.sql
+++ rasgotransforms/rasgotransforms/column_transforms/cast/cast.sql
@@ -1,5 +1,34 @@
+{%- macro get_source_col_names(source_table_fqtn=None) -%}
+    select * from {{ source_table_fqtn }} limit 0
+{%- endmacro -%}
+
+{%- if overwrite_columns is false -%}
+
 SELECT *
 {%- for target_col, type in casts.items() %}
     , CAST({{target_col}} AS {{type}}) AS {{cleanse_name(target_col)+'_'+cleanse_name(type)}}
 {%- endfor %}
 FROM {{ source_table }}
+
+{%- elif overwrite_columns is true -%}
+
+{%- set col_names_source_df = run_query(get_source_col_names(source_table_fqtn=source_table)) -%}
+{%- set source_col_names = col_names_source_df.columns.to_list() -%}
+
+{%- set cast_columns = [] -%}
+{%- for target_col, type in casts.items() -%}
+    {%- cast_columns.append(target_col) -%}
+{% endfor %}
+
+{%- set untouched_cols = source_col_names | reject('in', cast_columns) -%}
+
+SELECT
+{%- for col in untouched_cols %}
+    {{ ", " if not loop.first else " " }}{{ col }}
+{%- endfor %}
+{%- for target_col, type in casts.items() %}
+    , CAST({{target_col}} AS {{type}}) AS {{target_col}}
+{%- endfor %}
+FROM {{ source_table }}
+
+{%- endif -%}
