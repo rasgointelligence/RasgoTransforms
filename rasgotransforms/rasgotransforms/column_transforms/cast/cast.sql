ce78ee14db20b7e064182158cf19e39e9568e06c
Patrick <patrick@rasgoml.com>
2022-03-17 22:15:03 -0400

    fixed it

--- rasgotransforms/rasgotransforms/column_transforms/cast/cast.sql
+++ rasgotransforms/rasgotransforms/column_transforms/cast/cast.sql
@@ -2,7 +2,7 @@
     select * from {{ source_table_fqtn }} limit 0
 {%- endmacro -%}
 
-{%- if overwrite_columns is false -%}
+{%- if overwrite_columns == false -%}
 
 SELECT *
 {%- for target_col, type in casts.items() %}
@@ -10,15 +10,15 @@ SELECT *
 {%- endfor %}
 FROM {{ source_table }}
 
-{%- elif overwrite_columns is true -%}
+{%- elif overwrite_columns == true -%}
 
 {%- set col_names_source_df = run_query(get_source_col_names(source_table_fqtn=source_table)) -%}
 {%- set source_col_names = col_names_source_df.columns.to_list() -%}
 
 {%- set cast_columns = [] -%}
 {%- for target_col, type in casts.items() -%}
-    {%- cast_columns.append(target_col) -%}
-{% endfor %}
+    {{ cast_columns.append(target_col) or "" }}
+{%- endfor -%}
 
 {%- set untouched_cols = source_col_names | reject('in', cast_columns) -%}
 
