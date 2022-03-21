999cd45caac88347400646554947e2f399cfe6c9
Patrick <patrick@rasgoml.com>
2022-03-20 21:10:21 -0400

    add alias support for all dbs

--- rasgotransforms/rasgotransforms/column_transforms/datediff/datediff.sql
+++ rasgotransforms/rasgotransforms/column_transforms/datediff/datediff.sql
@@ -1,3 +1,9 @@
+{%- if alias is defined -%}
+    {%- set alias = cleanse_name(alias) -%}
+{%- else -%}
+    {%- set alias = 'DIFF_'~ cleanse_name(date_1~'_'~date_2) -%}
+{%- endif -%}
+
 SELECT *,
-  EXTRACT({{ date_part }} FROM DATE {{ date_1 }} - DATE {{ date_2 }}) AS {{ cleanse_name(date_2 + '_' + date_1 + '_datediff') }}
+  EXTRACT({{ date_part }} FROM DATE {{ date_1 }} - DATE {{ date_2 }}) AS {{ alias }}
 FROM {{ source_table }}
--- rasgotransforms/rasgotransforms/column_transforms/datediff/snowflake/datediff.sql
+++ rasgotransforms/rasgotransforms/column_transforms/datediff/snowflake/datediff.sql
@@ -1,3 +1,9 @@
+{%- if alias is defined -%}
+    {%- set alias = cleanse_name(alias) -%}
+{%- else -%}
+    {%- set alias = 'DIFF_'~ cleanse_name(date_1~'_'~date_2) -%}
+{%- endif -%}
+
 SELECT *,
-  DATEDIFF({{ date_part }}, {{ date_1 }}, {{ date_2 }}) as {{ cleanse_name(date_2 + '_' + date_1 + '_datediff') }}
+  DATEDIFF({{ date_part }}, {{ date_1 }}, {{ date_2 }}) as {{ alias }}
 FROM {{ source_table }}
