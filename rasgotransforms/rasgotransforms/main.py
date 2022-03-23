02b267a7eebe564e2138415fc6ed7bd898fc8081
SpencerZaghikianRasgoXPS15 <szaghikian@rasgoml.com>
2022-03-23 16:00:01 -0400

    support mysql

--- rasgotransforms/rasgotransforms/main.py
+++ rasgotransforms/rasgotransforms/main.py
@@ -13,32 +13,33 @@ import yaml
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-TRANSFORM_TYPES = [
-    'column',
-    'row',
-    'table'
-]
+TRANSFORM_TYPES = ["column", "row", "table"]
+
 
 class Datawarehouse(Enum):
     """
     Supported Data Warehouses
     """
-    BIGQUERY = 'bigquery'
-    SNOWFLAKE = 'snowflake'
-    POSTGRESQL = 'postgresql'
+
+    BIGQUERY = "bigquery"
+    SNOWFLAKE = "snowflake"
+    POSTGRESQL = "postgresql"
+    MYSQL = "mysql"
+
 
 class TransformTemplate:
     """
     Reference to a Rasgo transform template
     """
+
     def __init__(
-            self,
-            name: str,
-            arguments: List[dict],
-            source_code: str,
-            transform_type: str,
-            description: str = None
-        ):
+        self,
+        name: str,
+        arguments: List[dict],
+        source_code: str,
+        transform_type: str,
+        description: str = None,
+    ):
         self.name = name
         self.arguments = arguments
         self.source_code = source_code
@@ -46,23 +47,25 @@ class TransformTemplate:
         self.description = description
 
     def __repr__(self) -> str:
-        arg_str = ', '.join(f'{arg.get("name")}: {arg.get("type")}' for arg in self.arguments)
+        arg_str = ", ".join(
+            f'{arg.get("name")}: {arg.get("type")}' for arg in self.arguments
+        )
         return f"RasgoTemplate: {self.name}({arg_str})"
 
     def define(self) -> str:
         """
         Return a pretty string definition of this Transform
         """
-        pretty_string = f'{self.transform_type.title()} Transform: {self.name}' \
-            f'\nDescription: {self.description}' \
-            f'\nArguments: {self.arguments}' \
-            f'\nSourceCode: {self.source_code}'
+        pretty_string = (
+            f"{self.transform_type.title()} Transform: {self.name}"
+            f"\nDescription: {self.description}"
+            f"\nArguments: {self.arguments}"
+            f"\nSourceCode: {self.source_code}"
+        )
         return pretty_string
 
 
-def serve_rasgo_transform_templates(
-        datawarehouse: str
-    ) -> List[TransformTemplate]:
+def serve_rasgo_transform_templates(datawarehouse: str) -> List[TransformTemplate]:
     """
     Return a list of Rasgo Transform Templates
 
@@ -78,7 +81,7 @@ def serve_rasgo_transform_templates(
             transform_source_code = _get_transform_source_code(
                 transform_type=transform_type,
                 transform_name=transform_name,
-                datawarehouse=datawarehouse
+                datawarehouse=datawarehouse,
             )
             transform_args = _parse_transform_args_from_yaml(transform_yaml)
             template_list.append(
@@ -87,20 +90,20 @@ def serve_rasgo_transform_templates(
                     transform_type=transform_type,
                     source_code=transform_source_code,
                     arguments=transform_args,
-                    description=transform_yaml.get('description')
+                    description=transform_yaml.get("description"),
                 )
             )
     return template_list
 
-def _check_datawarehouse(
-        input_value: str
-    ) -> str:
-    supported_dws = "'"+"', '".join([e.value for e in Datawarehouse])+"'"
+
+def _check_datawarehouse(input_value: str) -> str:
+    supported_dws = "'" + "', '".join([e.value for e in Datawarehouse]) + "'"
     try:
         Datawarehouse[input_value.upper()]
     except Exception:
-        raise ValueError(f'datawarehouse parameter accepts values: {supported_dws}')
+        raise ValueError(f"datawarehouse parameter accepts values: {supported_dws}")
     return input_value.lower()
+
 
 def _get_root_dir() -> Path:
     """
@@ -108,27 +111,36 @@ def _get_root_dir() -> Path:
     """
     return Path(os.path.dirname(__file__))
 
+
 def _get_transform_source_code(
-        transform_type: str,
-        transform_name: str,
-        datawarehouse: str
-    ) -> str:
+    transform_type: str, transform_name: str, datawarehouse: str
+) -> str:
     """
     Return a transform's source code as a string
     """
     datawarehouse = _check_datawarehouse(datawarehouse)
     root_dir = _get_root_dir()
-    source_code_path = root_dir / f"{transform_type}_transforms" / transform_name / f"{transform_name}.sql"
-    source_code_override_path = root_dir / f"{transform_type}_transforms" / transform_name/ datawarehouse / f"{transform_name}.sql"
+    source_code_path = (
+        root_dir
+        / f"{transform_type}_transforms"
+        / transform_name
+        / f"{transform_name}.sql"
+    )
+    source_code_override_path = (
+        root_dir
+        / f"{transform_type}_transforms"
+        / transform_name
+        / datawarehouse
+        / f"{transform_name}.sql"
+    )
     if source_code_override_path.exists():
         source_code_path = source_code_override_path
     with open(source_code_path) as fp:
         source_code = fp.read()
     return source_code
 
-def _load_all_yaml_files(
-        datawarehouse: str
-    ) -> Dict[str, Dict[str, Dict]]:
+
+def _load_all_yaml_files(datawarehouse: str) -> Dict[str, Dict[str, Dict]]:
     """
     Load and return all the yaml files in the dir <root>/<transform_type>_transforms
     """
@@ -140,8 +152,15 @@ def _load_all_yaml_files(
 
         transform_names = [x.name for x in transform_type_dir_path.rglob("*/**")]
         for transform_name in transform_names:
-            transform_yaml_path = transform_type_dir_path / transform_name / f"{transform_name}.yaml"
-            transform_yaml_override_path = transform_type_dir_path / transform_name / datawarehouse / f"{transform_name}.yaml"
+            transform_yaml_path = (
+                transform_type_dir_path / transform_name / f"{transform_name}.yaml"
+            )
+            transform_yaml_override_path = (
+                transform_type_dir_path
+                / transform_name
+                / datawarehouse
+                / f"{transform_name}.yaml"
+            )
 
             if transform_yaml_override_path.exists():
                 transform_yaml_path = transform_yaml_override_path
@@ -154,24 +173,20 @@ def _load_all_yaml_files(
 
     return transform_yamls
 
-def _parse_transform_args_from_yaml(
-        transform_yaml: Dict
-    ) -> List[Dict[str, str]]:
+
+def _parse_transform_args_from_yaml(transform_yaml: Dict) -> List[Dict[str, str]]:
     """
     From a loaded Transform Yaml File parse the
     Transform args in proper format, return the args
     in proper format for transform creation in PyRasgo
     """
     transform_args = []
-    for arg_name, arg_meta_data in transform_yaml['arguments'].items():
-        transform_args.append(
-            {**{'name': arg_name}, **arg_meta_data}
-        )
+    for arg_name, arg_meta_data in transform_yaml["arguments"].items():
+        transform_args.append({**{"name": arg_name}, **arg_meta_data})
     return transform_args
 
-def _read_yaml(
-        yaml_path: Path
-    ) -> Dict:
+
+def _read_yaml(yaml_path: Path) -> Dict:
     """
     Read and load a YAML file into a dictionary
     """
@@ -179,5 +194,6 @@ def _read_yaml(
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as e:
-            logger.error(f"Error Parsing YAML file at {yaml_path}"
-                  f"\n\nError Msg: {e}")
+            logger.error(
+                f"Error Parsing YAML file at {yaml_path}" f"\n\nError Msg: {e}"
+            )
