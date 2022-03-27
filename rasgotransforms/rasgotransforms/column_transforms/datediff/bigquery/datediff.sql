1947c5d8be6e60bfb74903564063ce7881caf731
Patrick <patrick@rasgoml.com>
2022-03-27 10:17:37 -0400

    add alias support for datediff on all dbs (#138)
    * add alias support for all dbs
    
    * Added Auto Generated Transform docs
    
    Co-authored-by: GitHub <noreply@github.com>

--- rasgotransforms/rasgotransforms/column_transforms/datediff/bigquery/datediff.sql
+++ rasgotransforms/rasgotransforms/column_transforms/datediff/bigquery/datediff.sql
@@ -1,3 +1,9 @@
+{%- if alias is defined -%}
+    {%- set alias = cleanse_name(alias) -%}
+{%- else -%}
+    {%- set alias = 'DIFF_'~ cleanse_name(date_1~'_'~date_2) -%}
+{%- endif -%}
+
 SELECT *,
-  DATE_DIFF({{ date_1 }}, {{ date_2 }}, {{ date_part }}) as {{ cleanse_name(date_2 + '_' + date_1 + '_datediff') }}
+  DATE_DIFF({{ date_1 }}, {{ date_2 }}, {{ date_part }}) as {{ alias }}
 FROM {{ source_table }}
