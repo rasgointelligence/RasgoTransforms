bfc7e914fc484d64d1f3c72228b2a223195a2a7f
Patrick <patrick@rasgoml.com>
2022-04-08 15:32:59 -0400

    Update bar_chart.sql

--- rasgotransforms/rasgotransforms/transforms/bar_chart/bar_chart.sql
+++ rasgotransforms/rasgotransforms/transforms/bar_chart/bar_chart.sql
@@ -4,7 +4,11 @@ SELECT
 {%- for col, aggs in metrics.items() %}
         {%- set outer_loop = loop -%}
     {%- for agg in aggs %}
-    {{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
+        {%- if ' DISTINCT' in agg %}
+            {{ agg|replace(" DISTINCT", "") }}(DISTINCT {{ col }}) as {{ cleanse_name(col ~ '_' ~ agg) }}{{ '' if loop.last and outer_loop.last else ',' }}
+        {%- else -%}
+            {{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
+        {%- endif -%}
     {%- endfor -%}
 {%- endfor %}
 FROM {{ source_table }}
