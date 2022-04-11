ea8dcc865e7a094d798e407ed479fd3120097363
Patrick <patrick@rasgoml.com>
2022-04-11 14:15:32 -0400

    add correlation insight transform

---
+++ rasgotransforms/rasgotransforms/transforms/correlation/correlation.sql
@@ -0,0 +1,27 @@
+{%- macro get_col_names_types(source_table_fqtn=None) -%}
+    {%- set database, schema, table = source_table_fqtn.split('.') -%}
+    SELECT COLUMN_NAME, DATA_TYPE
+    FROM {{ database }}.information_schema.columns
+    WHERE TABLE_CATALOG = '{{ database|upper }}'
+    AND   TABLE_SCHEMA = '{{ schema|upper }}'
+    AND   TABLE_NAME = '{{ table|upper }}'
+{%- endmacro -%}
+{# Get all Columns in Source Table #}
+{%- set names_types_df = run_query(get_col_names_types(source_table_fqtn=source_table)) -%}
+{%- set names_types_list = names_types_df.set_index('COLUMN_NAME')['DATA_TYPE'].to_dict() -%}
+
+{%- set column_list = [] -%}
+
+{%- for key, value in names_types_list.items() -%}
+    {% if (value == 'NUMBER' or 'FLOAT' in value or 'INT' in value) %}
+    {%- do column_list.append(key) -%}
+    {%- endif -%}
+{%- endfor -%}
+
+{%- for combo in itertools.combinations(column_list, 2) -%}
+    SELECT '{{ combo[0] }}' as x_label,
+    '{{ combo[1] }}' as y_label,
+    CORR({{ combo[0] }}, {{ combo[1] }}) as Correlation
+    FROM {{ source_table }}
+    {% if not loop.last %} UNION {% endif -%}
+{%- endfor -%}
