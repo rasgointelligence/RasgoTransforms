378cc1df0a576af98890f112fae455281e2c0918
Kimball Hill <hillkimball@gmail.com>
2022-04-21 09:02:31 -0600

    merged replace and regexp_replace into one transform

--- rasgotransforms/rasgotransforms/transforms/replace_string/replace_string.sql
+++ rasgotransforms/rasgotransforms/transforms/replace_string/replace_string.sql
@@ -1,4 +1,27 @@
-SELECT *,
-REPLACE({{ source_col }}, '{{ pattern }}', '{{ replacement }}') AS {{target_col if target_col is defined else "REPLACE_" + source_col}}
-FROM {{ source_table }}
+{% if position is not defined %}
+    {% set position = 1 %}
+{% else %}
+    {% set use_regex = True %}
+{% endif %}
 
+{% if occurrence is not defined %}
+    {% set occurrence = 0 %}
+{% else %}
+    {% set use_regex = True %}
+{% endif %}
+
+{% if parameters is not defined %}
+    {% set parameters = 'c' %}
+{% else %}
+    {% set use_regex = True %}
+{% endif %}
+
+{% if use_regex %}
+SELECT *,
+REGEXP_REPLACE({{ source_col }}, '{{ pattern }}', '{{ replacement }}', {{ position }}, {{ occurrence }}, '{{ parameters }}') AS {{alias if alias is defined else "REPLACE_" + source_col}}
+FROM {{ source_table }}
+{% else %}
+SELECT *,
+REPLACE({{ source_col }}, '{{ pattern }}', '{{ replacement }}') AS {{alias if alias is defined else "REPLACE_" + source_col}}
+FROM {{ source_table }}
+{% endif %}
