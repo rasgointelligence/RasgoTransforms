af7af6a8921d85e963d3482b3645d73a26da449a
Kimball Hill <hillkimball@gmail.com>
2022-04-21 07:58:08 -0600

    filter table before sample

--- rasgotransforms/rasgotransforms/transforms/sample/sample.sql
+++ rasgotransforms/rasgotransforms/transforms/sample/sample.sql
@@ -4,8 +4,17 @@
     {%- set sample_amount = num_rows~' ROWS' -%}
 {% endif %}
 
+{% if filter_statements is defined %}
+WITH filtered AS (
+    SELECT * FROM {{source_table}}
+    {%- for filter_statement in filter_statements %}
+    {{ 'WHERE' if loop.first else 'AND' }} {{ filter_statement }}
+    {%- endfor -%}
+
+)
+SELECT * FROM filtered
+TABLESAMPLE BERNOULLI ( {{ sample_amount }} )
+{% else %}
 SELECT * FROM {{source_table}}
 TABLESAMPLE BERNOULLI ( {{ sample_amount }} )
-{%- for filter_statement in filter_statements %}
-{{ 'WHERE' if loop.first else 'AND' }} {{ filter_statement }}
-{%- endfor -%}
+{% endif %}
