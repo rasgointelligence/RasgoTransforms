0696a90887e55abb4446b22ae7b988161b438367
Kimball Hill <hillkimball@gmail.com>
2022-04-26 13:08:13 -0400

    made partition and order_by optional (#174)
    * made partition and order_by optional
    
    * updated docs to show optional args
    
    * Added Auto Generated Transform docs
    
    Co-authored-by: GitHub <noreply@github.com>

--- rasgotransforms/rasgotransforms/transforms/lag/lag.sql
+++ rasgotransforms/rasgotransforms/transforms/lag/lag.sql
@@ -1,8 +1,14 @@
-SELECT *, 
-    {% for col in columns %}
-        {%- for amount in amounts -%}
-            lag({{col}}, {{amount}}) over (partition by {{partition | join(", ")}} order by {{order_by | join(", ")}}) as Lag_{{ cleanse_name(col ~ '_' ~ amount) }}{{ ", " if not loop.last else "" }}
+{%- if partition is not defined or partition|length == 0 -%}
+{%- set partition = ["NULL"]-%}
+{%- endif -%}
+{%- if order_by is not defined or order_by|length == 0 -%}
+{%- set order_by = ["NULL"]-%}
+{%- endif -%}
+SELECT *,
+    {%- for col in columns -%}
+        {%- for amount in amounts %}
+        lag({{col}}, {{amount}}) over (partition by {{partition | join(", ")}} order by {{order_by | join(", ")}}) as Lag_{{ cleanse_name(col ~ '_' ~ amount) }}{{ "," if not loop.last else "" }}
         {%- endfor -%}
         {{ ", " if not loop.last else "" }}
-    {% endfor %}
+    {%- endfor %}
 from {{ source_table }}
