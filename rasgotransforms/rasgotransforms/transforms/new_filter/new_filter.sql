6d50358ffe7ff13206dc38c5bf545a6723bb0b72
Patrick <patrick@rasgoml.com>
2022-05-05 16:43:27 -0400

    new filter for UI

---
+++ rasgotransforms/rasgotransforms/transforms/new_filter/new_filter.sql
@@ -0,0 +1,11 @@
+SELECT *
+FROM {{ source_table }}
+{%- for filter_block in filter_statements %}
+{%- set oloop = loop -%}
+{{ 'WHERE ' if oloop.first else 'AND ' }}
+{% if 'advancedFilterString' in filter_block -%}
+{{ filter_block['advancedFilterString'] }}
+{%- else -%}
+{{ filter_block['columnName'] }} {{ filter_block['operator'] }} {{ filter_block['comparisonValue'] }}
+{%- endif -%}
+{%- endfor -%}
