5013af2f67a5358d081581b4df11e4750a13fb49
SpencerAtRasgo <szaghikian@rasgoml.com>
2022-05-06 12:05:08 -0400

    remove unused code

--- rasgotransforms/rasgotransforms/transforms/funnel/funnel.sql
+++ rasgotransforms/rasgotransforms/transforms/funnel/funnel.sql
@@ -1,4 +1,3 @@
--- For a single column ("LABEL") stage return:
 {%- for col_name in stage_columns -%}
     SELECT
     '{{ col_name }}' AS LABEL
@@ -6,10 +5,3 @@
 FROM {{ source_table }}
 {{ "UNION ALL" if not loop.last else "" }}
 {% endfor %}
-
--- OR, for a multi-column stage return:
--- SELECT
--- {%- for col_name in stage_columns -%}
--- {{ "," if not loop.first else "" }}SUM({{ col_name }})
--- {% endfor %}
--- FROM {{ source_table }}
