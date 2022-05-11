f7664e8754ef612d0aea486e039112143c01f7ce
Kimball Hill <hillkimball@gmail.com>
2022-05-11 13:19:03 -0600

    removed run_query for source_col lookup

--- rasgotransforms/rasgotransforms/transforms/correlation/correlation.sql
+++ rasgotransforms/rasgotransforms/transforms/correlation/correlation.sql
@@ -1,19 +1,9 @@
-{%- macro get_col_names_types(source_table_fqtn=None) -%}
-    {%- set database, schema, table = source_table_fqtn.split('.') -%}
-    SELECT COLUMN_NAME, DATA_TYPE
-    FROM {{ database }}.information_schema.columns
-    WHERE TABLE_CATALOG = '{{ database|upper }}'
-    AND   TABLE_SCHEMA = '{{ schema|upper }}'
-    AND   TABLE_NAME = '{{ table|upper }}'
-{%- endmacro -%}
-{# Get all Columns in Source Table #}
-{%- set names_types_df = run_query(get_col_names_types(source_table_fqtn=source_table)) -%}
-{%- set names_types_list = names_types_df.set_index('COLUMN_NAME')['DATA_TYPE'].to_dict() -%}
+{%- set names_types_list = get_columns(source_table) -%}
 
 {%- set column_list = [] -%}
 
 {%- for key, value in names_types_list.items() -%}
-    {% if (value == 'NUMBER' or 'FLOAT' in value or 'INT' in value) %}
+    {% if (value|upper == 'NUMBER' or 'FLOAT' in value|upper or 'INT' in value|upper) %}
     {%- do column_list.append(key) -%}
     {%- endif -%}
 {%- endfor -%}
--- rasgotransforms/rasgotransforms/transforms/drop_columns/drop_columns.sql
+++ rasgotransforms/rasgotransforms/transforms/drop_columns/drop_columns.sql
@@ -1,11 +1,3 @@
-{#
-Jinja Macro to generate a query that would get all 
-the columns in a table by fqtn
-#}
-{%- macro get_source_col_names(source_table_fqtn=None) -%}
-    select * from {{ source_table_fqtn }} limit 0
-{%- endmacro -%}
-
 {% if include_cols and exclude_cols is defined %}
 {{ raise_exception('You cannot pass both an include_cols list and an exclude_cols list') }}
 {% else %}
@@ -19,9 +11,7 @@ FROM {{source_table}}
 {%- endif -%}
 
 {%- if exclude_cols is defined -%}
-{# Get all Columns in Source Table #}
-{%- set col_names_source_df = run_query(get_source_col_names(source_table_fqtn=source_table)) -%}
-{%- set source_col_names = col_names_source_df.columns.to_list() -%}
+{%- set source_col_names = get_columns(source_table) -%}
 {%- set new_columns = source_col_names | reject('in', exclude_cols) -%}
 
 
