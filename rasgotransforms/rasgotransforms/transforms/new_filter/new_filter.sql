8fd2a65ca61440b7540b6779df77837a493bb97f
Patrick <patrick@rasgoml.com>
2022-05-11 09:33:21 -0400

    update the filter

--- rasgotransforms/rasgotransforms/transforms/new_filter/new_filter.sql
+++ rasgotransforms/rasgotransforms/transforms/new_filter/new_filter.sql
@@ -1,10 +1,10 @@
 SELECT *
 FROM {{ source_table }}
-{% for filter_block in filter_statements %}
+{% for filter_block in items %}
 {%- set oloop = loop -%}
 {{ 'WHERE ' if oloop.first else ' AND ' }}
-{% if 'advancedFilterString' in filter_block -%}
-{{ filter_block['advancedFilterString'] }}
+{%- if filter_block is not mapping -%}
+{{ filter_block }}
 {%- else -%}
     {%- if filter_block['operator'] == 'CONTAINS' -%}
 {{ filter_block['operator'] }}({{ filter_block['columnName'] }}, {{ filter_block['comparisonValue'] }})
