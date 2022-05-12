04779f02d605fee1326a6e68532479c939d212e7
Patrick <patrick@rasgoml.com>
2022-05-12 09:10:02 -0400

    publish the new filter

--- rasgotransforms/rasgotransforms/transforms/filter/filter.sql
+++ rasgotransforms/rasgotransforms/transforms/filter/filter.sql
@@ -1,4 +1,19 @@
-SELECT * FROM {{source_table}}
-{%- for filter_statement in filter_statements %}
-{{ 'WHERE' if loop.first else 'AND' }} {{ filter_statement }}
+{%- if items is not defined -%}
+    {%- set items = filter_statements -%}
+{%- endif -%}
+
+SELECT *
+FROM {{ source_table }}
+{% for filter_block in items %}
+{%- set oloop = loop -%}
+{{ 'WHERE ' if oloop.first else ' AND ' }}
+{%- if filter_block is not mapping -%}
+{{ filter_block }}
+{%- else -%}
+    {%- if filter_block['operator'] == 'CONTAINS' -%}
+{{ filter_block['operator'] }}({{ filter_block['columnName'] }}, {{ filter_block['comparisonValue'] }})
+    {%- else -%}
+{{ filter_block['columnName'] }} {{ filter_block['operator'] }} {{ filter_block['comparisonValue'] }}
+    {%- endif -%}
+{%- endif -%}
 {%- endfor -%}
