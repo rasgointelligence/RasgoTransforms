2adf954293c1d6aec7666c1112b61ba25ddb9d7e
Alex Wolf <alex.r.wolf22@gmail.com>
2022-05-16 11:46:24 -0500

    changed back name to join

--- rasgotransforms/rasgotransforms/transforms/join/join.sql
+++ rasgotransforms/rasgotransforms/transforms/join/join.sql
@@ -1,79 +1,33 @@
-{#
-Get two things in this loop
-    1. The column names of each base table in the Join
-    2. Mapping of fqtn -> table_name for all tables/FQTNs passed in
-#}
-{%- set source_col_names = [] -%}
-{%- set source_get_columns_keys = get_columns(source_table).keys() -%}
-{%- for col_name in source_get_columns_keys -%}
-    {%- set _x = source_col_names.append(col_name) -%}
-{%- endfor -%}
-{# Init Dict/List of lists we actaully need to get; fill it initially with source_table #}
-{%- set table_col_names = [['source_table', source_col_names]] -%}
-{%- set table_name_mapping = {"source_table": source_table.split('.')[-1]}-%}
-{%- for join_dict in join_dicts -%}
-    {%- set join_table_col_names = [] -%}
-    {%- set join_table_get_columns_keys = get_columns(join_dict['table_b']).keys() -%}
-    {%- for join_table_col_name in join_table_get_columns_keys -%}
-        {%- set _x = join_table_col_names.append(join_table_col_name) -%}
-    {%- endfor -%}
-    {%- set _x = table_col_names.append([join_dict['table_b'], join_table_col_names]) -%}
-    {%- set _x = table_name_mapping.__setitem__(
-        join_dict["table_b"], join_dict["table_b"].split('.')[-1]
-        ) 
-    -%}
-{%- endfor -%}
-
-{#
-For each table joining, figure out the columns which need to be prefixed
-#}
-{%- set cols_to_prefix_mapping = {} -%}
-{%- for join_table, join_table_cols in table_col_names[1:] -%}
-    {%- set cols_to_prefix = [] -%}
-    {%- set cols_to_check_for_overlap = [] -%}
-    {%- for i in range(0, loop.index) -%}
-        {%- set _x = cols_to_check_for_overlap.extend(table_col_names[i][1]) -%}
-    {%- endfor %}
-    {%- for join_table_col in join_table_cols -%}
-        {%- if join_table_col in cols_to_check_for_overlap -%}
-            {%- set _x = cols_to_prefix.append(join_table_col) -%}
-        {%- endif -%}
-    {%- endfor -%}
-    {%- set _x = cols_to_prefix_mapping.__setitem__(join_table, cols_to_prefix) -%}
-    {%- set cols_to_check_for_overlap = cols_to_check_for_overlap + join_table_cols -%}
-{%- endfor -%}
-
-{#
-Macro for getting the column selection in full JOIN statement
-
-This includes adding prefixes if we marked that column as needing a prefix
-#}
-{%- macro get_columns_to_select_in_query() -%}
-    {# First get the column selections for the source table #}
-    {%- set source_fqtn, source_col_names = table_col_names[0] -%}
-    {%- for source_col_name in source_col_names %}
-    {{ table_name_mapping[source_fqtn] }}.{{ source_col_name }},{% endfor -%}
-    {# Next get the column selections for the join tables #}
-    {%- for join_table, join_table_cols in table_col_names[1:] -%}
-        {%- set outer_loop = loop -%}
-        {%- set join_dict = join_dicts[loop.index0] -%}
-        {%- for join_table_col in join_table_cols -%}
-            {# Add prefix if we marked that column as needing a prefix #}
-            {%- if join_table_col in cols_to_prefix_mapping[join_table] %}
-    {{ table_name_mapping[join_table] }}.{{ join_table_col }} as {{ join_dict["join_prefix_b"] }}__{{ join_table_col }}{{ " " if outer_loop.last and loop.last else "," }}
-            {%- else %}
-    {{ table_name_mapping[join_table] }}.{{ join_table_col }}{{ " " if outer_loop.last and loop.last else "," }}
-            {%- endif -%}
-        {%- endfor -%}
-    {%- endfor -%}
+{# Jinja Macro to get the table name from source_id #}
+{%- macro get_table_name(join_table) -%}
+    {%- set table = join_table.split('.')[-1] -%}
+    {{ table }}
 {%- endmacro -%}
-{# Create the Final Join Statement #}
-SELECT {{get_columns_to_select_in_query()}}
-FROM {{source_table}}
-{% for join_dict in join_dicts -%}
-{%- set outer_loop = loop -%}
-{{ join_dict["join_type"] }} JOIN {{ join_dict["table_b"] }}
-ON {% for join_col1, join_col2 in join_dict["join_on"].items() -%}
-{{ " AND " if loop.index != 1 else "" }}{{ table_name_mapping["source_table"] if outer_loop.index == 1 else table_name_mapping[join_dict["table_a"]] }}.{{ join_col1 }} = {{ table_name_mapping[join_dict["table_b"]] }}.{{ join_col2 }}
+
+{# Get all Columns in Source Table #}
+{%- set source_col_names = get_columns(source_table) -%}
+
+{# Get all Columns and Table Name in Join Table #}
+{%- set join_col_names = get_columns(join_table) -%}
+{%- set join_table_name = get_table_name(join_table) -%}
+
+
+SELECT
+{%- for source_col in source_col_names %}
+  t1.{{ source_col }}{{ ', ' if not loop.last else '' }}
+{%- endfor -%}
+{%- for join_col in join_col_names %}
+    {%- if join_prefix -%}
+        , t2.{{ join_col }} as {{ cleanse_name(join_prefix)~'_'~join_col }}
+    {%- elif join_col not in source_col_names -%}
+        , t2.{{ join_col }}
+    {% endif %}
 {%- endfor %}
-{% endfor -%}
+FROM {{ source_table }} as t1
+{{ join_type + ' ' if join_type else '' | upper }}JOIN {{ join_table }} as t2
+{%- for t1_join_col, t2_join_col in join_columns.items() %}
+{{ ' AND' if not loop.first else 'ON'}} t1.{{ t1_join_col }} = t2.{{ t2_join_col }}
+{%- endfor -%}
+{%- for filter_statement in filters %}
+{{ 'WHERE ' if loop.first else ' AND ' }} {{ filter_statement }}
+{%- endfor -%}
