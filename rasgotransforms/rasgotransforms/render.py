109641701a749eb0b9f49962ce22701cf19ff7a9
Kimball Hill <hillkimball@gmail.com>
2022-05-23 13:08:39 -0600

    added documentation

--- rasgotransforms/rasgotransforms/render.py
+++ rasgotransforms/rasgotransforms/render.py
@@ -23,6 +23,9 @@ def get_dw_type(dw_type: str) -> Optional[DataWarehouse]:
 
 
 def get_path(transform_name: str, dw_type: Optional[DataWarehouse] = None) -> Optional[str]:
+    """
+    Gets the python file for the given transform specific to the given data warehouse if one exists
+    """
     root_dir = os.path.dirname(__file__)
     if dw_type:
         function_path = Path(root_dir, 'transforms', transform_name, dw_type.value, f'{transform_name}.py')
@@ -55,6 +58,10 @@ def cleanse_name(symbol: str) -> str:
 def infer_columns(
     transform_name: str, transform_args: Dict[str, Any], source_columns: Dict[str, str], dw_type: Optional[str] = None
 ) -> Optional[Dict[str, str]]:
+    """
+    Infers the column names and types in the output table from a query produced by a transform given
+    the transform name, args, source columns, and data warehouse type
+    """
     dw_type = get_dw_type(dw_type)
     function_path = get_path(transform_name, dw_type)
     if not function_path:
