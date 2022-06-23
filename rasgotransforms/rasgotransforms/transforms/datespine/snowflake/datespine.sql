c17beadda2f61cbb66b99cd98f3e5bef01e74d51
Kimball Hill <hillkimball@gmail.com>
2022-06-23 12:29:12 -0600

    improved datespine

--- rasgotransforms/rasgotransforms/transforms/datespine/snowflake/datespine.sql
+++ rasgotransforms/rasgotransforms/transforms/datespine/snowflake/datespine.sql
@@ -1,40 +1,48 @@
-{% set min_max_query %}
+{%- set min_max_query -%}
 select min(cast({{ date_col }} as date)) min_date, max(cast({{ date_col }} as date)) max_date from {{ source_table }}
-{% endset %}
-{% set min_max_query_result = run_query(min_max_query) %}
-{% if start_timestamp is defined %}
-    {% set min_date = start_timestamp %}
-{% else %}
-    {% set min_date = min_max_query_result[min_max_query_result.columns[0]][0] %}
-{% endif %}
-{% if  end_timestamp is defined %}
-    {% set max_date = end_timestamp %}
-{% else %}
-    {% set max_date = min_max_query_result[min_max_query_result.columns[1]][0] %}
-{% endif %}
-{% set row_count_query %}
-select datediff({{ interval_type }}, '{{ min_date }}'::timestamp_ntz, '{{ max_date }}'::timestamp_ntz)
-{% endset %}
-{% set row_count_query_results = run_query(row_count_query) %}
-{%- set row_count = row_count_query_results[row_count_query_results.columns[0]][0] -%}
-with date_spine as (
+{% endset -%}
+{% set min_max_query_result = run_query(min_max_query) -%}
+{% if start_timestamp is defined -%}
+    {% set min_date = start_timestamp -%}
+{% else -%}
+    {% set min_date = min_max_query_result[min_max_query_result.columns[0]][0] -%}
+{% endif -%}
+{% if  end_timestamp is defined -%}
+    {% set max_date = end_timestamp -%}
+{% else -%}
+    {% set max_date = min_max_query_result[min_max_query_result.columns[1]][0] -%}
+{% endif -%}
+{% set num_days = (max_date|string|todatetime - min_date|string|todatetime).days + 1 -%}
+with calendar as (
     select
-           row_number() over (order by null) as interval_id,
-            dateadd(
-               '{{ interval_type }}',
-               interval_id - 1,
-               '{{ min_date }}'::timestamp_ntz) as ts_ntz_interval_start,
-            dateadd(
-               '{{ interval_type }}',
-               interval_id,
-               '{{ min_date }}'::timestamp_ntz) as ts_ntz_interval_end
-from table (generator(rowcount => {{ row_count }}))
-    )
-select  {{ source_table }}.*,
-  date_spine.ts_ntz_interval_start as {{ date_col }}_spine_start,
-  date_spine.ts_ntz_interval_end as {{ date_col }}_spine_end
-from date_spine
-left join {{ source_table }} on
-    {{ source_table }}.{{ date_col }} >= date_spine.ts_ntz_interval_start
-    and
-    {{ source_table }}.{{ date_col }} < date_spine.ts_ntz_interval_end
+        row_number() over (order by null) as interval_id,
+        cast(dateadd(
+            'day',
+            interval_id-1,
+            '{{ min_date }}'::timestamp_ntz) as date) as date_day,
+        cast(date_trunc('week', date_day) as date) as date_week,
+        cast(date_trunc('month', date_day) as date) as date_month,
+        case
+            when month(date_day) in (1, 2, 3) then date_from_parts(year(date_day), 1, 1)
+            when month(date_day) in (4, 5, 6) then date_from_parts(year(date_day), 4, 1)
+            when month(date_day) in (7, 8, 9) then date_from_parts(year(date_day), 7, 1)
+            when month(date_day) in (10, 11, 12) then date_from_parts(year(date_day), 10, 1)
+        end as date_quarter,
+        cast(date_trunc('year', date_day) as date) as date_year
+    from table (generator(rowcount => {{ num_days }}))
+),
+spine as (
+    select distinct date_{{ interval_type }} as period
+    from calendar
+)
+select
+    cast(spine.period as timestamp) as {{ date_col }}_SPINE_START,
+    {%- if interval_type|lower == 'quarter' %}
+    dateadd('second', -1, dateadd('month',3, {{ date_col }}_SPINE_START)) as {{ date_col }}_SPINE_END,
+    {%- else %}
+    dateadd('second', -1, dateadd('{{ interval_type }}',1, {{ date_col }}_SPINE_START)) as {{ date_col }}_SPINE_END,
+    {%- endif %}
+    {{ source_table }}.*
+from spine
+left outer join {{ source_table }} on 
+    cast(date_trunc('{{ interval_type }}', cast({{ source_table}}.{{ date_col }} as date)) as date) = spine.period
