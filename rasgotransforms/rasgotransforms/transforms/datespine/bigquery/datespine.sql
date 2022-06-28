30d79028a13a35b5fc7df509936e2d9874460cfe
Kimball Hill <hillkimball@gmail.com>
2022-06-28 08:50:49 -0600

    improved datespine (#251)
    * improved datespine
    
    * improved datespine_groups
    
    * raise exception if run_query returns none and optional params arent filled

--- rasgotransforms/rasgotransforms/transforms/datespine/bigquery/datespine.sql
+++ rasgotransforms/rasgotransforms/transforms/datespine/bigquery/datespine.sql
@@ -1,40 +1,39 @@
-{% set min_max_query %}
+{% if start_timestamp is not defined or end_timestamp is not defined -%}
+{%- set min_max_query -%}
 select min(cast({{ date_col }} as date)) min_date, max(cast({{ date_col }} as date)) max_date from {{ source_table }}
-{% endset %}
-{% set min_max_query_result = run_query(min_max_query) %}
-{% if start_timestamp is defined %}
-    {% set min_date = start_timestamp %}
-{% else %}
-    {% set min_date = min_max_query_result[min_max_query_result.columns[0]][0] %}
-{% endif %}
-{% if  end_timestamp is defined %}
-    {% set max_date = end_timestamp %}
-{% else %}
-    {% set max_date = min_max_query_result[min_max_query_result.columns[1]][0] %}
-{% endif %}
-{% set row_count_query %}
-select DATE_DIFF(cast('{{ min_date }}' as date), cast('{{ max_date }}' as date), {{ interval_type }})
-{% endset %}
-{% set row_count_query_results = run_query(row_count_query) %}
-{%- set row_count = row_count_query_results[row_count_query_results.columns[0]][0] -%}
-with date_spine as (
-    select
-           row_number() over (order by null) as interval_id,
-            DATE_ADD(
-                cast('{{ min_date }}' as timestamp),
-                INTERVAL interval_id - 1 {{ interval_type }}
-                ) as ts_ntz_interval_start,
-            DATE_ADD(
-                cast('{{ min_date }}' as timestamp)
-                INTERVAL interval_id {{ interval_type }}
-               ) as ts_ntz_interval_end
-from table (generator(rowcount => {{ row_count }}))
-    )
-select  {{ source_table }}.*,
-  date_spine.ts_ntz_interval_start as {{ date_col }}_spine_start,
-  date_spine.ts_ntz_interval_end as {{ date_col }}_spine_end
-from date_spine
-left join {{ source_table }} on
-    {{ source_table }}.{{ date_col }} >= date_spine.ts_ntz_interval_start
-    and
-    {{ source_table }}.{{ date_col }} < date_spine.ts_ntz_interval_end
+{% endset -%}
+{% set min_max_query_result = run_query(min_max_query) -%}
+{% if min_max_query_result is none -%}
+{{ raise_exception('start_timstamp and end_timestamp must be provided when no Data Warehouse connection is available')}}
+{% endif -%}
+{% endif -%}
+{% if start_timestamp is defined -%}
+    {% set min_date = start_timestamp -%}
+{% else -%}
+    {% set min_date = min_max_query_result[min_max_query_result.columns[0]][0] -%}
+{% endif -%}
+{% if  end_timestamp is defined -%}
+    {% set max_date = end_timestamp -%}
+{% else -%}
+    {% set max_date = min_max_query_result[min_max_query_result.columns[1]][0] -%}
+{% endif -%}
+with calendar as (
+  select
+    date_day,
+    date_trunc(date_day, week) as date_week,
+    date_trunc(date_day, month) as date_month,
+    date_trunc(date_day, quarter) as date_quarter,
+    date_trunc(date_day, year) as date_year,
+    from unnest(generate_date_array('{{ min_date }}', '{{ max_date }}')) as date_day
+),
+spine as (
+    select distinct date_{{ interval_type }} as period
+    from calendar
+)
+select
+    cast(spine.period as timestamp) as {{ date_col }}_SPINE_START,
+    timestamp_add(cast(date_add(spine.period, INTERVAL 1 {{ interval_type }}) as timestamp), INTERVAL -1 second) as {{ date_col }}_SPINE_END,
+    {{ source_table }}.*
+from spine
+left outer join {{ source_table }} on 
+    cast(date_trunc(cast({{ source_table}}.{{ date_col }} as date), {{ interval_type }}) as date) = spine.period
