e7ceb0024a49dcf3ddaf175c3419cd38e6ec1e26
Kimball Hill <hillkimball@gmail.com>
2022-07-05 13:57:14 -0600

    fix last item deleted bug

--- rasgotransforms/rasgotransforms/transforms/drop_columns/drop_columns.sql
+++ rasgotransforms/rasgotransforms/transforms/drop_columns/drop_columns.sql
@@ -2,6 +2,19 @@
 {{ raise_exception('You cannot pass both an include_cols list and an exclude_cols list') }}
 {% else %}
 
+{%- if exclude_cols is defined -%}
+{%- set source_col_names = get_columns(source_table) -%}
+
+{# Upper exclude cols to ensure case insensitive name matching #}
+{%- set exclude_cols = (exclude_cols|join(',')|upper).split(',') -%}
+{% set include_cols = [] -%}
+{% for column_name in source_col_names -%}
+    {% if column_name.upper() not in exclude_cols -%}
+         {% do include_cols.append(column_name) -%}
+    {% endif -%}
+{% endfor -%}
+{%- endif -%}
+
 {%- if include_cols is defined -%}
 SELECT
 {%- for col in include_cols %}
@@ -10,19 +23,4 @@ SELECT
 FROM {{source_table}}
 {%- endif -%}
 
-{%- if exclude_cols is defined -%}
-{%- set source_col_names = get_columns(source_table) -%}
-
-{# Upper exclude cols to ensure case insensitive name matching #}
-{%- set exclude_cols = (exclude_cols|join(',')|upper).split(',') -%}
-
-SELECT
-{% for column_name in source_col_names %}
-    {%- if column_name.upper() not in exclude_cols -%}
-        {{ "\t"+column_name }}{{ ", \n" if not loop.last else " " }}
-    {%- endif -%}
-{% endfor %}
-FROM {{ source_table }}
-
-{%- endif -%}
 {%- endif -%}
