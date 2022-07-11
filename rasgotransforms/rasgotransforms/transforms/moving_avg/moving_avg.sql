23719e5aab0e9301ed4b780cd31831572b055602
SpencerAtRasgo <szaghikian@rasgoml.com>
2022-07-11 14:06:38 -0400

    clarify moving avg

--- rasgotransforms/rasgotransforms/transforms/moving_avg/moving_avg.sql
+++ rasgotransforms/rasgotransforms/transforms/moving_avg/moving_avg.sql
@@ -1,3 +1,8 @@
+{%- for amount in window_sizes -%}
+    {%- if amount < 0 -%}
+        {{ raise_exception('Cannot use negative values for a moving average. Please only pass positive values in `window_sizes`.') }}
+    {%- endif -%}
+{%- endfor -%}
 SELECT * 
 {%- for column in input_columns -%}
     {%- for window in window_sizes -%}
