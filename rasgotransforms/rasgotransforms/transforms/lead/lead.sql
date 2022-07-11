33a85ea1452650afa0b829296f6bf95cee8374c3
SpencerAtRasgo <szaghikian@rasgoml.com>
2022-07-11 13:18:45 -0400

    need a lead since we can't just do negative value lags now

---
+++ rasgotransforms/rasgotransforms/transforms/lead/lead.sql
@@ -0,0 +1,14 @@
+{%- if partition is not defined or partition|length == 0 -%}
+{%- set partition = ["NULL"]-%}
+{%- endif -%}
+{%- if order_by is not defined or order_by|length == 0 -%}
+{%- set order_by = ["NULL"]-%}
+{%- endif -%}
+SELECT *,
+    {%- for col in columns -%}
+        {%- for amount in amounts %}
+        lead({{col}}, {{amount}}) over (partition by {{partition | join(", ")}} order by {{order_by | join(", ")}}) as lead_{{ cleanse_name(col ~ '_' ~ amount) }}{{ "," if not loop.last else "" }}
+        {%- endfor -%}
+        {{ ", " if not loop.last else "" }}
+    {%- endfor %}
+from {{ source_table }}
