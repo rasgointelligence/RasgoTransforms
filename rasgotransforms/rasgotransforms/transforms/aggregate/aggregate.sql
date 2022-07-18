14c121c5ef0841c40fbc3d3e52f8866af9d8283e
szaghikian-rasgo <87394236+szaghikian-rasgo@users.noreply.github.com>
2022-07-18 12:55:13 -0400

    uppercase agg before replacing distinct
    Co-authored-by: Kimball Hill <hillkimball@gmail.com>

--- rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.sql
+++ rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.sql
@@ -7,7 +7,7 @@ SELECT
     {%- set outer_loop = loop -%}
     {%- for agg in aggs %}
         {%- if ' DISTINCT' in agg|upper %}
-            {{ agg|replace(" DISTINCT", "") }}(DISTINCT {{ col }}) as {{ col ~ '_' ~ agg|replace(" DISTINCT", "") ~ 'DISTINCT'}}{{ '' if loop.last and outer_loop.last else ',' }}
+            {{ agg|upper|replace(" DISTINCT", "") }}(DISTINCT {{ col }}) as {{ col ~ '_' ~ agg|upper|replace(" DISTINCT", "") ~ 'DISTINCT'}}{{ '' if loop.last and outer_loop.last else ',' }}
         {%- else %}
             {{ agg }}({{ col }}) as {{ col + '_' + agg }}{{ '' if loop.last and outer_loop.last else ',' }}
         {%- endif %}
