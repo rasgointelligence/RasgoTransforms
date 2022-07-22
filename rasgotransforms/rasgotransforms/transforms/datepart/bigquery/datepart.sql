1d8592070afadc15144377bfc2e48ffbbc9dfae3
SpencerAtRasgo <szaghikian@rasgoml.com>
2022-07-22 11:30:15 -0400

    bin and datepart for BQ

---
+++ rasgotransforms/rasgotransforms/transforms/datepart/bigquery/datepart.sql
@@ -0,0 +1,16 @@
+SELECT *,
+{%- for target_col, date_part in dates.items() %}
+  {%- if date_part|lower == 'weekiso' %}
+    EXTRACT(ISOWEEK FROM {{ target_col }}) AS {{ target_col }}_ISOWEEK {{ ", " if not loop.last else "" }}
+  {%- elif date_part|lower == 'dayofweekiso' %}
+    MOD(EXTRACT(DAYOFWEEK FROM {{ target_col }}) + 5, 7) + 1 AS {{ target_col }}_ISODAYOFWEEK {{ ", " if not loop.last else "" }}
+    EXTRACT(ISOYEAR FROM {{ target_col }}) AS {{ target_col }}_ISOYEAR {{ ", " if not loop.last else "" }}
+  {%- elif date_part|lower == 'yearofweekiso' %}
+    EXTRACT(ISOYEAR FROM {{ target_col }}) AS {{ target_col }}_ISOYEAR {{ ", " if not loop.last else "" }}
+  {%- elif date_part|lower == 'yearofweek' %}
+    EXTRACT(YEAR FROM {{ target_col }}) AS {{ target_col }}_YEAR {{ ", " if not loop.last else "" }}
+  {%- else %}
+    EXTRACT({{ date_part }} FROM {{ target_col }}) AS {{ target_col }}_{{ date_part }} {{ ", " if not loop.last else "" }}
+  {%- endif %}
+{%- endfor %}
+FROM {{ source_table }}
