71c08f57ce373b66503c3a2207e0b2a864667aca
Patrick <patrick@rasgoml.com>
2022-07-25 10:08:41 -0400

    Update joins (#285)
    * Update joins
    
    fixing some bugs in joins()
    making code more modular
    
    * Added Auto Generated Transform docs
    
    Co-authored-by: GitHub <noreply@github.com>

--- rasgotransforms/rasgotransforms/transforms/joins/joins.sql
+++ rasgotransforms/rasgotransforms/transforms/joins/joins.sql
@@ -1,74 +1,74 @@
-{#
-Get two things in this loop
-    1. The column names of each base table in the Join
-    2. Mapping of fqtn -> table_name for all tables/FQTNs passed in
-#}
 {%- macro table_from_fqtn(fqtn) -%}
     {{ fqtn.split('.')[-1] }}
 {%- endmacro -%}
-{%- set source_col_names = [] -%}
-{%- set source_get_columns_keys = get_columns(source_table).keys() -%}
-{%- for col_name in source_get_columns_keys -%}
-    {%- set _x = source_col_names.append(col_name) -%}
-{%- endfor -%}
-{# Init Dict/List of lists we actaully need to get; fill it initially with source_table #}
-{%- set table_col_names = [['source_table', source_col_names]] -%}
+
+{# Create global variables to track query components #}
+{%- set ns = namespace() -%}
+{%- set ns.alias_columns=[] -%}
+{%- set ns.select_columns='' -%}
+{%- set ns.all_columns={} -%}
+{%- set ns.all_tables=[] -%}
+{%- set ns.columns_to_check = [] -%}
+
+{# assemble lists of all columns and all tables in the query using a loop #}
 {%- for join_dict in join_dicts -%}
-    {%- set join_table_col_names = [] -%}
-    {%- set join_table_get_columns_keys = get_columns(join_dict['table_b']).keys() -%}
-    {%- for join_table_col_name in join_table_get_columns_keys -%}
-        {%- set _x = join_table_col_names.append(join_table_col_name) -%}
-    {%- endfor -%}
-    {%- set _x = table_col_names.append([join_dict['table_b'], join_table_col_names]) -%}
+    {%- if loop.index == 1 -%}
+        {%- set base_table = source_table -%}
+    {%- else -%}
+        {%- set base_table = join_dict['table_a'] -%}
+    {%- endif -%}
+    {%- set jtable = join_dict['table_b'] -%}
+    {# Check if base_table in the running list of all tables yet or not. If not, we need to add it and add its columns to all_columns. #}
+    {%- if base_table not in ns.all_tables  -%}
+        {%- set base_cols = get_columns(base_table) -%}
+        {%- set x=ns.all_columns.__setitem__(base_table, base_cols.keys()|list) -%}
+    {%- endif -%}
+    {%- if jtable not in ns.all_tables  -%}
+        {%- set jtable_cols = get_columns(jtable) -%}
+        {%- set x=ns.all_columns.__setitem__(jtable, jtable_cols.keys()|list) -%}
+    {%- endif -%}
+    {%- set ns.all_tables = ns.all_tables + [base_table, jtable] -%}
 {%- endfor -%}
-{# For each table joining, figure out the columns which need to be prefixed #}
-{%- set cols_to_prefix_mapping = {} -%}
-{%- for join_table, join_table_cols in table_col_names[1:] -%}
-    {%- set cols_to_prefix = [] -%}
-    {%- set cols_to_check_for_overlap = [] -%}
-    {%- for i in range(0, loop.index) -%}
-        {%- set _x = cols_to_check_for_overlap.extend(table_col_names[i][1]) -%}
-    {%- endfor %}
-    {%- for join_table_col in join_table_cols -%}
-        {%- if join_table_col in cols_to_check_for_overlap -%}
-            {%- set _x = cols_to_prefix.append(join_table_col) -%}
+
+{# loop through all columns in all tables to check for column names that are repeated between tables. These need to be aliased. #}
+{%- for fqtn, columns in ns.all_columns.items() -%}
+    {%- set ns.columns_to_check = [] -%}
+    {%- for check_fqtn in ns.all_tables -%}
+        {%- if fqtn != check_fqtn -%}
+            {%- set ns.columns_to_check = ns.columns_to_check + ns.all_columns[check_fqtn] -%}
         {%- endif -%}
     {%- endfor -%}
-    {%- set _x = cols_to_prefix_mapping.__setitem__(join_table, cols_to_prefix) -%}
-    {%- set cols_to_check_for_overlap = cols_to_check_for_overlap + join_table_cols -%}
+    {%- set columns_to_alias = columns|select("in", ns.columns_to_check)|list -%}
+    {%- set ns.alias_columns = ns.alias_columns + columns_to_alias -%}
 {%- endfor -%}
-{#
-Macro for getting the column selection in full JOIN statement; This includes adding prefixes if we marked that column as needing a prefix
-#}
-{%- macro get_columns_to_select_in_query() -%}
-    {# First get the column selections for the source table #}
-    {%- set source_fqtn, source_col_names = table_col_names[0] -%}
-    {%- for source_col_name in source_col_names %}
-    {{ table_from_fqtn(source_table) }}.{{ source_col_name }},{% endfor -%}
-    {# Next get the column selections for the join tables #}
-    {%- for join_table, join_table_cols in table_col_names[1:] -%}
-        {%- set outer_loop = loop -%}
-        {%- set join_dict = join_dicts[loop.index0] -%}
-        {%- for join_table_col in join_table_cols -%}
-            {# Add prefix if we marked that column as needing a prefix #}
-            {%- if join_table_col in cols_to_prefix_mapping[join_table] %}
-    {{ table_from_fqtn(join_table) }}.{{ join_table_col }} as {{ join_dict["join_prefix_b"] }}__{{ join_table_col }}{{ " " if outer_loop.last and loop.last else "," }}
-            {%- else %}
-    {{ table_from_fqtn(join_table) }}.{{ join_table_col }}{{ " " if outer_loop.last and loop.last else "," }}
-            {%- endif -%}
-        {%- endfor -%}
+
+{# assemble the SELECT clause by aliasing columns that need to be aliased and just using column name otherwise. #}
+{%- for fqtn, columns in ns.all_columns.items() -%}
+    {%- set o_loop = loop -%}
+    {%- for column in columns -%}
+    {%- set table_prefix = table_from_fqtn(fqtn) -%}
+        {%- if column in ns.alias_columns -%}
+            {%- set ns.select_columns = ns.select_columns ~ table_prefix ~ '.' ~ column ~ ' AS ' ~ table_prefix ~ '_' ~ column -%}
+        {%- else -%}
+            {%- set ns.select_columns = ns.select_columns ~ ' ' ~ column -%}
+        {%- endif -%}
+        {%- if not (loop.last and o_loop.last) -%}
+            {%- set ns.select_columns = ns.select_columns ~ ', ' -%}
+        {%- endif -%}
     {%- endfor -%}
-{%- endmacro -%}
-{# Create the Final Join Statement #}
-SELECT {{get_columns_to_select_in_query()}}
+{%- endfor -%}
+
+{# assemble the full query #}
+SELECT {{ ns.select_columns }}
 FROM {{ source_table }}
 {% for join_dict in join_dicts %}
-{%- set outer_loop = loop -%}
-{{ join_dict["join_type"] }} JOIN {{ join_dict["table_b"] }}
-{%- if join_dict["join_type"]|upper != 'CROSS' %}
- ON {%- for join_col1, join_col2 in join_dict["join_on"].items() -%}
-{{ " AND " if loop.index != 1 else "" }}
-{{ table_from_fqtn(source_table) if outer_loop.index == 1 else table_from_fqtn(join_dict["table_a"]) }}.{{ join_col1 }} = {{ table_from_fqtn(join_dict["table_b"]) }}.{{ join_col2 }}
-{% endfor %}
-{%- endif -%}
-{% endfor -%}
+    {%- set outer_loop = loop -%}
+    {{ join_dict["join_type"] }} JOIN {{ join_dict["table_b"] }}
+    {%- if join_dict["join_type"]|upper != 'CROSS' %}
+    ON
+    {%- for join_col1, join_col2 in join_dict["join_on"].items() -%}
+        {{ " AND " if loop.index != 1 else "" }}
+        {{ table_from_fqtn(source_table) if outer_loop.index == 1 else table_from_fqtn(join_dict["table_a"]) }}.{{ join_col1 }} = {{ table_from_fqtn(join_dict["table_b"]) }}.{{ join_col2 }}
+    {% endfor %}
+    {%- endif -%}
+{%- endfor -%}
