5e81182e5dee6f796952b0007b80242e4e750b3b
SpencerAtRasgo <szaghikian@rasgoml.com>
2022-07-26 15:21:35 -0400

    impute stuff for bigquery

--- rasgotransforms/rasgotransforms/transforms/replace_missing/bigquery/replace_missing.sql
+++ rasgotransforms/rasgotransforms/transforms/replace_missing/bigquery/replace_missing.sql
@@ -4,15 +4,19 @@ for a specified imputation col
 
 If strategy is not mean, median, or mode
 make query to fill with supplied scalar value
-else it will perform that impuattion stagety on column
+else it will perform that imputation stagety on column
 #}
 {%- macro get_impute_query(col, imputation) -%}
     {%- set impute_expression = '' -%}
     {%- set imputation_strategy = '' -%}
-    {%- if imputation | lower in  ['mean', 'median', 'mode', 'max', 'min', 'sum'] -%}
+    {%- if imputation | lower in  ['mean', 'max', 'min', 'sum'] -%}
         {%- set imputation = 'AVG' if imputation == 'mean' else imputation -%}
         {%- set imputation_strategy = imputation | upper -%}
         {%- set impute_expression = imputation_strategy + '(' + col + ') over ()' -%}
+    {%- elif imputation|lower == "median" -%}
+        {%- set impute_expression = 'PERCENTILE_CONT(' + col + ', 0.5) over ()' -%}
+    {%- elif imputation|lower == "mode" -%}
+        {%- set impute_expression = col + '_MODE_VALUE' -%}
     {%- else -%}
         {%- set imputation = "'" + imputation + "'" if imputation is string else imputation -%}
         {%- set impute_expression = imputation -%}
@@ -32,15 +36,36 @@ else it will perform that impuattion stagety on column
 {# Get all Columns in Source Table #}
 {%- set source_col_names = get_columns(source_table) -%}
 
+{%- set mode_aggs = dict() -%}
+{%- for col, agg in replacements.items() -%}
+    {%- if 'MODE' in agg|upper -%}
+        {%- set _ = mode_aggs.update({col: agg}) -%}
+    {%- endif -%}
+{%- endfor -%}
+
+{%- if mode_aggs %}
+    {%- for mode_col, mode_agg in mode_aggs.items() %}
+        WITH {{ mode_col }}_MODE_CTE AS (
+            SELECT
+                APPROX_TOP_COUNT({{ mode_col }}, 1)[OFFSET(0)].VALUE AS {{ mode_col }}_MODE_VALUE
+            FROM {{ source_table }}
+        ){{ ',' if not loop.last else '' }}
+    {%- endfor %}
+{%- endif %}
 SELECT
 {%- for col in source_col_names -%}
     {%- if col in replacements %}
-    {{ get_impute_query(col, replacements[col]) }}{{ ',' if flag_missing_vals or not loop.last else ''}}
-    {%- if flag_missing_vals %}
-    {{ get_flag_missing_query(col) }}{{ ',' if not loop.last else ''}}
-    {%- endif -%}
+        {{ get_impute_query(col, replacements[col]) }}{{ ',' if flag_missing_vals or not loop.last else ''}}
+        {%- if flag_missing_vals %}
+            {{ get_flag_missing_query(col) }}{{ ',' if not loop.last else ''}}
+        {%- endif -%}
     {%- else %}
-    {{ col }}{{ ',' if not loop.last else ''}}
+        {{ col }}{{ ',' if not loop.last else ''}}
     {%- endif -%}
 {%- endfor %}
 FROM {{source_table}}
+{%- if mode_aggs %}
+    {%- for mode_col, mode_agg in mode_aggs.items() %}
+        ,{{ mode_col }}_MODE_CTE
+    {%- endfor %}
+{%- endif %}
