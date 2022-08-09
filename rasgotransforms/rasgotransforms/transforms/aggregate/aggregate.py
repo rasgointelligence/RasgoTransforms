c4e60c454c277653c40cc3fbaf893d1ce3dfff56
Kimball Hill <hillkimball@gmail.com>
2022-08-09 11:11:41 -0600

    fix agg column inference (#301)
    * fix agg column inference
    
    * bump version
    
    * abstract numeric types

--- rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.py
+++ rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.py
@@ -1,12 +1,34 @@
-# INT_AGGREGATIONS = [
-#     'AVG', 'CORR', 'COUNT',
-# ]
+NUMERIC_TYPES = [
+    'int',
+    'integer',
+    'bigint',
+    'smallint',
+    'number',
+    'numeric',
+    'float',
+    'float4',
+    'float8',
+    'decimal',
+    'double precision',
+    'real',
+]
 
 
 def infer_columns(args, source_columns) -> dict:
+    args = args.copy()
     out_cols = {}
     for col in args['group_by']:
         out_cols[col] = source_columns[col.upper()]
+    if 'numeric columns' in args['aggregations'].keys():
+        for column, column_type in source_columns.items():
+            if column not in args['aggregations'].keys() and column_type.lower() in NUMERIC_TYPES:
+                args['aggregations'].setdefault(column, []).extend(args['aggregations']['numeric columns'])
+        args['aggregations'].pop('numeric columns')
+    if 'nonnumeric columns' in args['aggregations'].keys():
+        for column, column_type in source_columns.items():
+            if column not in args['aggregations'].keys() and column_type.lower() not in NUMERIC_TYPES:
+                args['aggregations'].setdefault(column, []).extend(args['aggregations']['nonnumeric columns'])
+        args['aggregations'].pop('nonnumeric columns')
     for col in args['aggregations'].keys():
         for agg in args['aggregations'][col]:
             agg = agg.replace(' ', '')
--- rasgotransforms/rasgotransforms/version.py
+++ rasgotransforms/rasgotransforms/version.py
@@ -1,4 +1,4 @@
 """
 Package version for pypi
 """
-__version__ = "2.1.1"
+__version__ = "2.1.2"
