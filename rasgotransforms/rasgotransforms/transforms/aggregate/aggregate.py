b074182476170f3ea169b553edb2b334409687bf
Kimball Hill <hillkimball@gmail.com>
2022-08-09 11:03:48 -0600

    fix agg column inference

--- rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.py
+++ rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.py
@@ -4,9 +4,46 @@
 
 
 def infer_columns(args, source_columns) -> dict:
+    args = args.copy()
     out_cols = {}
     for col in args['group_by']:
         out_cols[col] = source_columns[col.upper()]
+    if 'numeric columns' in args['aggregations'].keys():
+        for column, column_type in source_columns.items():
+            if column not in args['aggregations'].keys() and column_type.lower() in [
+                'int',
+                'integer',
+                'bigint',
+                'smallint',
+                'number',
+                'numeric',
+                'float',
+                'float4',
+                'float8',
+                'decimal',
+                'double precision',
+                'real',
+            ]:
+                args['aggregations'].setdefault(column, []).extend(args['aggregations']['numeric columns'])
+        args['aggregations'].pop('numeric columns')
+    if 'nonnumeric columns' in args['aggregations'].keys():
+        for column, column_type in source_columns.items():
+            if column not in args['aggregations'].keys() and column_type.lower() not in [
+                'int',
+                'integer',
+                'bigint',
+                'smallint',
+                'number',
+                'numeric',
+                'float',
+                'float4',
+                'float8',
+                'decimal',
+                'double precision',
+                'real',
+            ]:
+                args['aggregations'].setdefault(column, []).extend(args['aggregations']['nonnumeric columns'])
+        args['aggregations'].pop('nonnumeric columns')
     for col in args['aggregations'].keys():
         for agg in args['aggregations'][col]:
             agg = agg.replace(' ', '')
