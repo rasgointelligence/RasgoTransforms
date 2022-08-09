61f5b1df27890b586ad945c7ed025b5f1f0e115a
Kimball Hill <hillkimball@gmail.com>
2022-08-09 10:29:51 -0600

    Aggregate UI changes (#300)
    * edited clean to be compatible with new ui field
    
    * allow users to pass numeric and non numeric global aggregate options
    
    * don't get columns twice

--- rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.sql
+++ rasgotransforms/rasgotransforms/transforms/aggregate/aggregate.sql
@@ -1,3 +1,24 @@
+{%- set aggregations = aggregations.copy() %}
+{%- if 'numeric columns' in aggregations.keys() and aggregations['numeric columns']|length > 0 %}
+    {%- set all_columns = get_columns(source_table) %}
+    {%- for column, column_type in all_columns.items() %}
+        {%- if column not in aggregations.keys() and column_type|lower in ['int', 'integer', 'bigint', 'smallint', 'number', 'numeric', 'float', 'float4', 'float8', 'decimal', 'double precision', 'real'] %}
+            {%- do aggregations.setdefault(column, []).extend(aggregations['numeric columns']) %}
+        {%- endif %}
+    {%- endfor %}
+    {%- set _ = aggregations.pop('numeric columns') %}
+{%- endif -%}
+
+{%- if 'nonnumeric columns' in aggregations.keys() and aggregations['nonnumeric columns']|length > 0 %}
+    {%- set all_columns = all_columns if all_columns is defined else get_columns(source_table) %}
+    {%- for column, column_type in all_columns.items() %}
+        {%- if column not in aggregations.keys() and column_type|lower not in ['int', 'integer', 'bigint', 'smallint', 'number', 'numeric', 'float', 'float4', 'float8', 'decimal', 'double precision', 'real'] %}
+            {%- do aggregations.setdefault(column, []).extend(aggregations['nonnumeric columns']) %}
+        {%- endif %}
+    {%- endfor %}
+    {%- set _ = aggregations.pop('nonnumeric columns') %}
+{%- endif -%}
+
 SELECT
 {%- for group_item in group_by %}
     {{ group_item }},
