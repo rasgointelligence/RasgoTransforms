672977da8a3fb9841685491fa7a79831f73aba5f
Kimball Hill <hillkimball@gmail.com>
2022-08-12 12:01:03 -0600

    fixed conditional and timeseries agg

--- rasgotransforms/rasgotransforms/transforms/timeseries_agg/timeseries_agg.sql
+++ rasgotransforms/rasgotransforms/transforms/timeseries_agg/timeseries_agg.sql
@@ -1,22 +1,26 @@
-SELECT *
-{% for offset in offsets -%}
-  {% set normalized_offset = -offset %}
-  {% for col, aggs in aggregations.items() -%}
+select
+    *
+    {% for offset in offsets %}
+    {% set normalized_offset = -offset %}
+    {% for col, aggs in aggregations.items() %}
     {% for agg in aggs %}
-    ,(
-      SELECT {{ agg }}({{ col }})
-      FROM {{ source_table }} i  
-      WHERE 
-      {% if normalized_offset > 0 -%}
-        i.{{ date }} BETWEEN o.{{ date }} AND (o.{{ date }} + INTERVAL {{ normalized_offset }} {{ date_part }})
-      {% else -%}
-        i.{{ date }} BETWEEN (o.{{ date }} - INTERVAL {{ normalized_offset|abs }} {{ date_part }})) AND o.{{ date }}
-      {%- endif -%}
-      {%- for g in group_by %}
-        AND o.{{ g }} = i.{{ g }} 
-      {% endfor -%}
-    ) AS {{ cleanse_name(agg + '_' + col + '_' + offset|string + date_part) }}
-    {%- endfor -%}
-  {%- endfor %}
-{% endfor %}
-FROM {{ source_table }} o
+    ,
+    (
+        select {{ agg }} ({{ col }})
+        from {{ source_table }} i
+        where
+            {% if normalized_offset > 0 %}
+            i.{{ date }} between o.{{ date }} and (
+                o.{{ date }} + interval {{ normalized_offset }} {{ date_part }}
+            )
+            {% else %}
+            i.{{ date }} between (
+                o.{{ date }} - interval {{ normalized_offset|abs }} {{ date_part }}
+            ) and o.{{ date }}
+            {% endif %}
+            {% for g in group_by %} and o.{{ g }} = i.{{ g }} {% endfor %}
+    ) as {{ cleanse_name(agg + '_' + col + '_' + offset|string + date_part) }}
+    {% endfor %}
+    {% endfor %}
+    {% endfor %}
+from {{ source_table }} o
