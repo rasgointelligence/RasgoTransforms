90d1f7aad2262f2294d7231f9bb2eec9224364ef
Kimball Hill <hillkimball@gmail.com>
2022-08-23 12:43:38 -0600

    added coumpoundBoolean to filter transform

--- rasgotransforms/rasgotransforms/transforms/filter/filter.sql
+++ rasgotransforms/rasgotransforms/transforms/filter/filter.sql
@@ -5,19 +5,25 @@
         {%- set items = filter_statements -%}
     {%- endif -%}
 {%- endif -%}
+{% set logical_operator = namespace(value='AND') %}
+{% for filter_block in items %}
+    {% if filter_block is mapping and 'compoundBoolean' in filter_block %}
+        {% set logical_operator.value = filter_block['compoundBoolean'] %}
+    {% endif %}
+{% endfor %}
 
 SELECT *
 FROM {{ source_table }}
-{% for filter_block in items %}
+WHERE
+{%- for filter_block in items %}
 {%- set oloop = loop -%}
-{{ 'WHERE ' if oloop.first else ' AND ' }}
-{%- if filter_block is not mapping -%}
-{{ filter_block }}
-{%- else -%}
-    {%- if filter_block['operator'] == 'CONTAINS' -%}
-{{ filter_block['operator'] }}({{ filter_block['columnName'] }}, {{ filter_block['comparisonValue'] }})
-    {%- else -%}
-{{ filter_block['columnName'] }} {{ filter_block['operator'] }} {{ filter_block['comparisonValue'] }}
-    {%- endif -%}
-{%- endif -%}
+    {%- if filter_block is not mapping %}
+    {{ logical_operator.value if not loop.first}} {{ filter_block }}
+    {%- else %}
+        {%- if filter_block['operator'] == 'CONTAINS' %}
+    {{ logical_operator.value if not loop.first}} {{ filter_block['operator'] }}({{ filter_block['columnName'] }}, {{ filter_block['comparisonValue'] }})
+        {%- else %}
+    {{ logical_operator.value if not loop.first}} {{ filter_block['columnName'] }} {{ filter_block['operator'] }} {{ filter_block['comparisonValue'] }}
+        {%- endif %}
+    {%- endif %}
 {%- endfor -%}
