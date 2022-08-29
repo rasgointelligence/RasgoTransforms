bdbc95bb01a74a140a4f553e1bf27cee04f4fa42
Kimball Hill <hillkimball@gmail.com>
2022-08-29 17:15:31 -0600

    accept namespace objects as filters

--- rasgotransforms/rasgotransforms/macros/filter.sql
+++ rasgotransforms/rasgotransforms/macros/filter.sql
@@ -5,12 +5,20 @@
 {% else %}
 {% set logical_operator = namespace(value='AND') %}
 {% for filter in filters %}
-    {% if filter is mapping and 'compoundBoolean' in filter %}
-        {% set logical_operator.value = filter['compoundBoolean'] %}
+    {% if filter is not string %}
+        {% if filter is not mapping %}
+            {% set filter = dict(filter) %}
+        {% endif %}
+        {% if filter is mapping and 'compoundBoolean' in filter and filter['compoundBoolean'] %}
+            {% set logical_operator.value = filter['compoundBoolean'] %}
+        {% endif %}
     {% endif %}
 {% endfor %}
 (
     {% for filter in filters %}
+    {% if filter is not string and filter is not mapping %}
+    {% set filter = dict(filter) %}
+    {% endif %}
     {% if filter is not mapping %}
     {{ logical_operator.value + ' ' if not loop.first }}{{ filter }}
     {% elif filter.operator|upper == 'CONTAINS' %}
--- rasgotransforms/rasgotransforms/version.py
+++ rasgotransforms/rasgotransforms/version.py
@@ -1,4 +1,4 @@
 """
 Package version for pypi
 """
-__version__ = "2.2.2a1"
+__version__ = "2.2.2"
