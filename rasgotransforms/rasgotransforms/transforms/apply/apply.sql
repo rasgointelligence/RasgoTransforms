36f3a6d5b97969ec85ae7d6b27289c43714074fe
Kimball Hill <hillkimball@gmail.com>
2022-09-06 12:18:51 -0600

    adjust declared args to match yaml configs

--- rasgotransforms/rasgotransforms/transforms/apply/apply.sql
+++ rasgotransforms/rasgotransforms/transforms/apply/apply.sql
@@ -1,3 +1,7 @@
-{# Placeholder code. Will be replaced by user supplied template #} 
+{# Placeholder code. Will be replaced by user supplied template #}
+{% if sql %}
+{{ sql }}
+{% else %}
 SELECT * FROM {{ source_table }}
+{% endif %}
 {{ raise_exception('Placeholder code must be replaced by user supplied template') }}
