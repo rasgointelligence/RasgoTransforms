0a492c018515b3e999df4aca9fe6c453e0c41f65
griff <70294474+griffatrasgo@users.noreply.github.com>
2022-09-30 10:32:03 -0400

    nobody could possible hate jinja as much as i do

--- rasgotransforms/rasgotransforms/snippets/sort_dict.sql
+++ rasgotransforms/rasgotransforms/snippets/sort_dict.sql
@@ -1,3 +1,3 @@
-{%- for column, direction in sort_dict.items() -%}
+{% for column, direction in sort_dict.items() -%}
   {{ column }} {{ direction }}{%- if not loop.last %}, {% endif -%}
 {%- endfor %}
--- rasgotransforms/rasgotransforms/version.py
+++ rasgotransforms/rasgotransforms/version.py
@@ -1,4 +1,4 @@
 """
 Package version for pypi
 """
-__version__ = "2.2.9"
+__version__ = "2.2.10"
