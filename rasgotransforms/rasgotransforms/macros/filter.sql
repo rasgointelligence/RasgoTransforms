7df0a981c24d4c15b6a52e194eb80afd75f12b04
Kimball Hill <hillkimball@gmail.com>
2022-10-06 16:50:03 -0600

    support both contracts

--- rasgotransforms/rasgotransforms/macros/filter.sql
+++ rasgotransforms/rasgotransforms/macros/filter.sql
@@ -2,15 +2,15 @@
 
 {% for filter in filters %}
 {% set condition = filter.condition + ' ' if not loop.first else '' %}
-{% if filter.type == 'sql' %}
+{% if filter.type|lower == 'sql' %}
 {{ condition }}{{ filter.value }}
-{% elif filter.type == 'object' %}
-{% if filter.operator|upper == 'CONTAINS' %}
+{% elif filter.type|lower == 'object' %}
+{% if filter.operator|lower == 'contains' %}
 {{ condition}}{{ filter.value.operator }}({{ filter.value.columnName }}, {{ filter.value.comparisonValue }})
 {% else %}
 {{ condition}}{{ filter.value.columnName }} {{ filter.value.operator }} {{ filter.value.comparisonValue }}
 {% endif %}
-{% elif filter.type == 'group' %}
+{% elif filter.type|lower == 'group' %}
 {{ condition }}(
         {{ get_filter_group(filter.value) | indent(8) }}
     )
@@ -19,10 +19,49 @@
 
 {% endmacro %}
 
+{% macro get_old_filter_statement(filters) %}
+{% if filters %}
+{% if filters is string %}
+{{ filters }}
+{% else %}
+{% set logical_operator = namespace(value='AND') %}
+{% for filter in filters %}
+    {% if filter is not string %}
+        {% if filter is not mapping %}
+            {% set filter = dict(filter) %}
+        {% endif %}
+        {% if filter is mapping and 'compoundBoolean' in filter and filter['compoundBoolean'] %}
+            {% set logical_operator.value = filter['compoundBoolean'] %}
+        {% endif %}
+    {% endif %}
+{% endfor %}
+(
+    {% for filter in filters %}
+    {% if filter is not string and filter is not mapping %}
+    {% set filter = dict(filter) %}
+    {% endif %}
+    {% if filter is not mapping %}
+    {{ logical_operator.value + ' ' if not loop.first }}{{ filter }}
+    {% elif filter.operator|upper == 'CONTAINS' %}
+    {{ logical_operator.value + ' ' if not loop.first }}{{ filter.operator }}({{ filter.columnName }}, {{ filter.comparisonValue }})
+    {% else %}
+    {{ logical_operator.value + ' ' if not loop.first }}{{ filter.columnName }} {{ filter.operator }} {{ filter.comparisonValue }}
+    {% endif %}
+    {% endfor %}
+)
+{% endif %}
+{% else %}
+true
+{% endif %}
+{% endmacro %}
 
 {% macro get_filter_statement(filters) %}
 {% if filters %}
+{% if 'condition' not in filters[0] %}
+{{ get_old_filter_statement(filters) }}
+{% else %}
 {{ get_filter_group(filters) }}
+{% endif %}
 {% else %}
 true
 {% endif %}
--- rasgotransforms/rasgotransforms/version.py
+++ rasgotransforms/rasgotransforms/version.py
@@ -1,4 +1,4 @@
 """
 Package version for pypi
 """
-__version__ = "2.2.11a1"
+__version__ = "2.2.11a2"
