9bd577ceffe7eafb2bf9416908553c4a9407a0e3
Kimball Hill <hillkimball@gmail.com>
2022-10-26 14:41:57 -0600

    fixed filter snippet

--- rasgotransforms/rasgotransforms/snippets/filter_list.sql
+++ rasgotransforms/rasgotransforms/snippets/filter_list.sql
@@ -1,20 +1,21 @@
-{% if filter_list %}
-{% set filter_list = {'filters': filter_list, 'logical_operator': 'and' } %}
-{% for filter in filter_list.filters %}
+{% set filters = filter_list %}
+{% if filters %}
+{% set filters = {'filters': filters, 'logical_operator': 'and' } %}
+{% for filter in filters.filters %}
     {% if filter is not string %}
         {% if filter is mapping and 'compoundBoolean' in filter and filter['compoundBoolean'] %}
-            {% do filter_list.__setitem__('logical_operator', filter['compoundBoolean']) %}
+            {% do filters.__setitem__('logical_operator', filter['compoundBoolean']) %}
         {% endif %}
     {% endif %}
 {% endfor %}
 (
-    {% for filter in filter_list.filters %}
+    {% for filter in filters.filters %}
     {% if filter is not mapping %}
-    {{ filter_list.logical_operator + ' ' if not loop.first }}{{ filter }}
+    {{ filters.logical_operator + ' ' if not loop.first }}{{ filter }}
     {% elif filter.operator|upper == 'CONTAINS' %}
-    {{ filter_list.logical_operator + ' ' if not loop.first }}{{ filter.operator }}({{ filter.columnName }}, {{ filter.comparisonValue }})
+    {{ filters.logical_operator.value + ' ' if not loop.first }}{{ filter.columnName }} like '%{{ filter.comparisonValue }}%'
     {% else %}
-    {{ filter_list.logical_operator + ' ' if not loop.first }}{{ filter.columnName }} {{ filter.operator }} {{ filter.comparisonValue }}
+    {{ filters.logical_operator + ' ' if not loop.first }}{{ filter.columnName }} {{ filter.operator }} {{ filter.comparisonValue }}
     {% endif %}
     {% endfor %}
 )
--- rasgotransforms/rasgotransforms/version.py
+++ rasgotransforms/rasgotransforms/version.py
@@ -1,4 +1,4 @@
 """
 Package version for pypi
 """
-__version__ = "2.2.12"
+__version__ = "2.2.13"
