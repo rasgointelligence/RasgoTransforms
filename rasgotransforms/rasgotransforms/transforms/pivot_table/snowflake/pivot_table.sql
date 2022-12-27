73e5eb4840135b4ebe80ab9c3b859d7530a39d0d
Patrick <patrick@rasgoml.com>
2022-12-27 15:08:16 -0500

    make it work

--- rasgotransforms/rasgotransforms/transforms/pivot_table/snowflake/pivot_table.sql
+++ rasgotransforms/rasgotransforms/transforms/pivot_table/snowflake/pivot_table.sql
@@ -1,22 +1,17 @@
+{% from 'filter.sql' import get_filter_statement %}
+
 {%- set distinct_val_query -%}
-select distinct {{ value_column }}
+select distinct {{ columns }}
 from {{ source_table }}
 limit 1000
 {%- endset -%}
 
-{%- set run_query_error_message -%}
-This transform depends on dynamic values to work, but no Data Warehouse connection is available. 
-Instead, please use the `list_of_vals` argument to provide these values explicitly
-{%- endset -%}
-
-{%- if list_of_vals is not defined -%}
+{%- if columns is defined -%}
     {%- set results = run_query(distinct_val_query) -%}
     {%- if results is none -%}
-        {{ raise_exception(run_query_error_message) }}
+        {{ raise_exception('Unable to successfully retrieve distinct values from your data warehouse.') }}
     {%- endif -%}
     {%- set distinct_vals = results[results.columns[0]].to_list() -%}
-{%- else -%}
-    {%- set distinct_vals = list_of_vals -%}
 {%- endif -%}
 
 {# Jinja Macro to get the comma separated cleansed name list #}
@@ -26,8 +21,41 @@ Instead, please use the `list_of_vals` argument to provide these values explicit
 {%- endfor -%}
 {%- endmacro -%}
 
+{%- if rows is defined -%}
+    {%- set group_by = rows|join(', ') ~ ', '  -%}
+{%- else -%}
+    {%- set group_by = '' -%}
+{%- endif -%}
 
-SELECT {{ dimensions | join(", ") }}{{ ',' if dimensions else ''}} {{ get_values(distinct_vals) }}
-FROM ( SELECT {{ dimensions | join(", ") }}{{ ',' if dimensions else ''}} {{ pivot_column }}, {{ value_column }} FROM {{ source_table }})
-PIVOT ( {{ agg_method }} ( {{ pivot_column }} ) FOR {{ value_column }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p
-( {{ dimensions | join(", ") }}{{ ',' if dimensions else ''}} {{ get_values(distinct_vals) }} )
+WITH filtered as (
+    SELECT *
+    FROM {{ source_table }}
+    {%- if filters is defined %}
+    where true AND
+    {{ get_filter_statement(filters) | indent }}
+    {%- endif %}
+)
+
+{%- if columns is defined -%}
+
+SELECT 
+    {{ group_by }}
+    {{ get_values(distinct_vals) }}
+FROM ( SELECT
+    {{ group_by }}
+    {{ values }}, 
+    {{ columns }}
+    FROM filtered)
+PIVOT ( {{ agg_method }} ( {{ values }} ) FOR {{ columns }} IN ( '{{ distinct_vals | join("', '") }}' ) ) as p
+(   {{ group_by }}
+    {{ get_values(distinct_vals) }} )
+
+{%- else -%}
+
+SELECT
+    {{ group_by }}
+    {{ agg_method }} ( {{ values }} )
+FROM filtered
+    GROUP BY {{ rows | join(', ') }}
+
+{%- endif -%}
