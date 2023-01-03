aa513923b9506e1710811fb8715eaebacf5134ab
griffatrasgo <70294474+griffatrasgo@users.noreply.github.com>
2023-01-02 23:23:07 -0500

    Relative Date Filters (#340)
    * first try
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    * bump version - alpha
    
    * fix bq pivot_table
    
    * bump version
    
    * fix dateadd
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- rasgotransforms/rasgotransforms/macros/filter.sql
+++ rasgotransforms/rasgotransforms/macros/filter.sql
@@ -16,19 +16,40 @@
 {% endfor %}
 (
     {% for filter in filters %}
+
     {% if filter is not string and filter is not mapping %}
     {% set filter = dict(filter) %}
     {% endif %}
+
     {% if 'columnName' in filter %}
         {% do filter.__setitem__('column_name', filter.columnName) %}
     {% endif %}
     {% if 'comparisonValue' in filter %}
         {% do filter.__setitem__('comparison_value', filter.comparisonValue) %}
     {% endif %}
+
+    {% if filter.comparison_value is mapping %}
+        {% if filter.comparison_value.type == 'RELATIVEDATE' or filter.comparison_value.type == 'relativedate' %}
+            {% if 'datePart' in filter.comparison_value %}
+                {% do filter.comparison_value.__setitem__('date_part', filter.datePart) %}
+            {% endif %}
+            {% if filter.comparison_value.direction == 'past' %}
+                {% do filter.comparison_value.__setitem__('offset', -filter.comparison_value.offset)  %}
+            {% endif %}
+            {% if dw_type() == 'bigquery' %}
+                {% do filter.__setitem__('comparison_value', "DATE_ADD(CURRENT_DATE, INTERVAL " ~ filter.comparison_value.offset ~ " " ~ filter.comparison_value.date_part ~ ")") %}
+            {% elif dw_type() == 'snowflake' %}
+                {% do filter.__setitem__('comparison_value', "DATEADD(" ~ filter.comparison_value.date_part ~ "," ~ filter.comparison_value.offset ~ ", CURRENT_DATE)") %}
+            {% else %}
+                {% do filter.__setitem__('comparison_value', "(CURRENT_DATE + INTERVAL '" ~ filter.comparison_value.offset ~ " " ~ filter.comparison_value.date_part ~ "')") %}
+            {% endif %}
+        {% endif %}
+    {% endif %}
+
     {% if filter is not mapping %}
     {{ logical_operator.value + ' ' if not loop.first }}{{ filter }}
     {% elif filter.operator|upper == 'CONTAINS' %}
-    {{ logical_operator.value + ' ' if not loop.first }}{{ filter.column_name }} like '%{{ filter.comparison_value }}%'
+    {{ logical_operator.value + ' ' if not loop.first }}{{ filter.column_name }} LIKE '%{{ filter.comparison_value }}%'
     {% else %}
     {{ logical_operator.value + ' ' if not loop.first }}{{ filter.column_name }} {{ filter.operator }} {{ filter.comparison_value }}
     {% endif %}
