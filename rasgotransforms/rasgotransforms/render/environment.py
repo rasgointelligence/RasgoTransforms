acadf6b0f44e2ba2ff69e5b6ea90f8dee0731cfb
Kimball Hill <hillkimball@gmail.com>
2023-01-06 14:16:18 -0700

    make start of period vs relative date more explicit

--- rasgotransforms/rasgotransforms/render/environment.py
+++ rasgotransforms/rasgotransforms/render/environment.py
@@ -155,26 +155,60 @@ def get_timedelta(time_grain: str, interval: int = None) -> timedelta:
         raise RenderException(f"Invalid time grain '{time_grain}'")
 
 
+def time_from_start_of_period(time_grain: str) -> timedelta:
+    """
+    Helper method that returns (current time - start of period/time grain)
+    """
+    time_grain = time_grain.lower()
+    current_time = datetime.utcnow()
+    if time_grain == 'hour':
+        return current_time - current_time.replace(minute=0, second=0, microsecond=0)
+    elif time_grain == 'day':
+        return current_time - current_time.replace(minute=0, second=0, microsecond=0)
+    elif time_grain == 'week':
+        return timedelta(
+            days=current_time.isoweekday(),
+            hours=current_time.hour,
+            minutes=current_time.minute,
+            seconds=current_time.second,
+            microseconds=current_time.microsecond,
+        )
+    elif time_grain == 'month':
+        return current_time - current_time.replace(day=1, minute=0, second=0, microsecond=0)
+    elif time_grain == 'quarter':
+        return current_time - current_time.replace(
+            month=((current_time.month - 1) // 3) * 3 + 1, day=1, minute=0, second=0, microsecond=0
+        )
+    elif time_grain == 'year':
+        return current_time - current_time.replace(month=1, day=1, minute=0, second=0, microsecond=0)
+    else:
+        raise RenderException(f"Invalid time grain '{time_grain}'")
+
+
 def parse_comparison_value(comparison_value, dw_type: DataWarehouse):
     if not isinstance(comparison_value, dict):
         return quote(comparison_value)
-    if comparison_value['type'].lower() == 'relativedate':
-        date_part = comparison_value.get('date_part', comparison_value.get('datePart'))
+    direction = comparison_value.get('direction', 'none').lower()
+    offset = comparison_value.get('offset', 0) or 0
+    if direction == 'past':
+        comparison_value['offset'] = -abs(offset)
+    else:
+        comparison_value['offset'] = abs(offset)
+    date_part = comparison_value.get('date_part', comparison_value.get('datePart'))
+    if comparison_value['type'].lower() == 'relative_date':
         if dw_type == DataWarehouse.SNOWFLAKE:
-            if not comparison_value['offset']:
-                return f"DATE_TRUNC({date_part}, CURRENT_DATE)"
-            else:
-                return f"DATEADD({date_part}, {comparison_value['offset']}, CURRENT_DATE)"
+            return f"DATEADD({date_part}, {comparison_value['offset']}, CURRENT_DATE)"
         elif dw_type == DataWarehouse.BIGQUERY:
-            if not comparison_value['offset']:
-                return f"DATE_TRUNC(CURRENT_DATE, {date_part})"
-            else:
-                return f"DATE_ADD(CURRENT_DATE, INTERVAL {comparison_value['offset']} {date_part})"
+            return f"DATE_ADD(CURRENT_DATE, INTERVAL {comparison_value['offset']} {date_part})"
         else:
-            if not comparison_value['offset']:
-                return f"DATE_TRUNC({date_part}, CURRENT_DATE)"
-            else:
-                return f"(CURRENT_DATE + INTERVAL {comparison_value['offset']} {date_part})"
+            return f"(CURRENT_DATE + INTERVAL {comparison_value['offset']} {date_part})"
+    elif comparison_value['type'].lower() == 'start_of_period':
+        if dw_type == DataWarehouse.SNOWFLAKE:
+            return f"DATE_TRUNC({date_part}, CURRENT_DATE)"
+        elif dw_type == DataWarehouse.BIGQUERY:
+            return f"DATE_TRUNC(CURRENT_DATE, {date_part})"
+        else:
+            return f"DATE_TRUNC({date_part}, CURRENT_DATE)"
     else:
         raise RenderException(f"Invalid comparison value object type '{comparison_value['type']}'")
 
@@ -200,13 +234,23 @@ def adjust_start_date(start_date, time_grain, secondary_calculations, dw_type: D
             max_timedelta = max(max_timedelta, get_timedelta(calc['period'], 1))
 
     if isinstance(start_date, dict):
+        direction = start_date.get('direction', 'none').lower()
+        offset = start_date.get('offset', 0)
+        if direction == 'past':
+            start_date['offset'] = -abs(offset)
+        else:
+            start_date['offset'] = abs(offset)
         date_part = start_date.get('date_part', start_date.get('datePart', None))
-        time_delta = get_timedelta(date_part, start_date['offset']) - max_timedelta
+        if start_date['type'].lower() == 'start_of_period':
+            time_delta = time_from_start_of_period(date_part)
+        else:
+            time_delta = get_timedelta(date_part, start_date['offset']) - max_timedelta
         return parse_comparison_value(
             {
-                'type': 'relativedate',
+                'type': 'relative_date',
                 'offset': time_delta.days,
                 'date_part': 'DAY',
+                'direction': 'past' if time_delta.days < 0 else 'future',
             },
             dw_type=dw_type,
         )
