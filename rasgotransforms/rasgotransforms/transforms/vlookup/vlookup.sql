59ed590935884446bbf2d7ed22da842a66623d89
Patrick <patrick@rasgoml.com>
2023-01-21 22:31:14 -0500

    add vlookup transform
    we <3 excel

---
+++ rasgotransforms/rasgotransforms/transforms/vlookup/vlookup.sql
@@ -0,0 +1,27 @@
+{%- macro table_from_fqtn(fqtn) -%}
+    {{ fqtn.split('.')[-1] }}
+{%- endmacro -%}
+
+{# Get all Columns in Source Table #}
+{%- set source_col_names = get_columns(source_table) -%}
+
+{# Get relevant Columns and Table Name in Lookup Table #}
+{%- if keep_columns is defined -%}
+    {%- set lookup_table_cols = keep_columns -%}
+{%- else -%}
+    {%- set lookup_table_cols = get_columns(lookup_table) -%}
+{%- endif -%}
+{%- set lookup_table_name = table_from_fqtn(lookup_table) -%}
+
+
+SELECT base.*, 
+{%- for column in lookup_table_cols %}
+    {%- if column in source_col_names -%}
+        lookup.{{ column }} as {{ lookup_table_name }}_{{ column }}{{ ', ' if not loop.last }}
+    {%- else -%}
+        {{ column }}{{ ', ' if not loop.last }}
+    {%- endif -%}
+{%- endfor %}
+FROM {{ source_table }} base
+LEFT OUTER JOIN {{ lookup_table }} lookup
+on base.{{ lookup_column }} = lookup.{{ lookup_column }}
