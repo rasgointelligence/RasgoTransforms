0dc02164217303f45946b572cfa61862a8f70f6b
Patrick <patrick@rasgoml.com>
2023-02-03 08:42:31 -0500

    make it

---
+++ rasgotransforms/rasgotransforms/transforms/new_columns/new_columns.sql
@@ -0,0 +1,9 @@
+SELECT *
+{%- for col_dict in calculated_columns %}
+    {%- if 'alias' in col_dict -%}
+    , {{ col_dict['calculated_column'] }} as {{ cleanse_name(col_dict['alias']) }}
+    {%- else -%}
+    , {{ col_dict['calculated_column'] }}
+    {%- endif %}
+{%- endfor %}
+FROM {{ source_table }}
