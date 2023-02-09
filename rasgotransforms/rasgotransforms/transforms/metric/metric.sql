9ef46ccd260767f29b582c40b1bf4e8fea170598
Kimball Hill <hillkimball@gmail.com>
2023-02-09 15:54:06 +0100

    Metric update (#361)
    * removed pivot step
    
    * made changes to args
    
    * refactor to make more sense
    
    * alpha version
    
    * fix all timegrain
    
    * make publish action manual (#362)
    
    * make publish action manual
    
    * allow transforms to not use source table (accelerators)
    
    * fix utils
    
    * revert utils bug

--- rasgotransforms/rasgotransforms/transforms/metric/metric.sql
+++ rasgotransforms/rasgotransforms/transforms/metric/metric.sql
@@ -1,14 +1,12 @@
 {% from 'aggregate_metrics.sql' import calculate_timeseries_metric_values %}
 {% from 'expression_metrics.sql' import calculate_expression_metric_values %}
-{% from 'distinct_values.sql' import get_distinct_vals %}
 {% from 'pivot.sql' import pivot_plot_values %}
 {% from 'filter.sql' import get_filter_statement, combine_filters %}
 {% from 'secondary_calculation.sql' import render_secondary_calculations %}
 {% set dimensions = group_by_dimensions if group_by_dimensions is defined else [] %}
-{% set max_num_groups = max_num_groups if max_num_groups is defined else 10 %}
 {% set filters = filters if filters is defined else [] %}
-{% set expression_metric_names = [] %}
-{% set expression_metrics = [] %}
+{% set unique_metric_names = [] %}
+{% set unique_metrics = [] %}
 {% set secondary_calculations = [] %}
 {% if metrics %}
     {% set metrics = cleanse_keys(metrics) %}
@@ -21,24 +19,29 @@
         {# Lookup metric if receiving key instead of metric dict #}
         {% do comparison.update(ref_metric(comparison.resource_key)) %}
     {% endif %}
-    {% if comparison.name not in expression_metric_names %}
-        {% do expression_metric_names.append(comparison.name) %}
-        {% do expression_metrics.append(comparison) %}
+    {% if comparison.name not in unique_metric_names %}
+        {% do unique_metric_names.append(comparison.name) %}
+        {% do unique_metrics.append(comparison) %}
     {% endif %}
     {% if comparison.secondary_calculation is defined and comparison.secondary_calculation.type|lower != 'default' %}
         {% do comparison.secondary_calculation.__setitem__('metric_names', [comparison.name]) %}
         {% do secondary_calculations.append(comparison.secondary_calculation) %}
     {% endif %}
 {% endfor %}
-{% set original_start_date = parse_comparison_value(timeseries_options.start_date) %}
-{% set end_date = 'CURRENT_DATE' if not timeseries_options.end_date else parse_comparison_value(timeseries_options.end_date) %}
-{% set time_grain = 'day' if not timeseries_options.time_grain else timeseries_options.time_grain %}
-{% set start_date = adjust_start_date(start_date=timeseries_options.start_date, time_grain=time_grain, secondary_calculations=secondary_calculations) %}
-{% set expression_metrics = combine_metrics(expression_metrics) %}
+{% if date_settings.start_date == '' %}
+{% do date_settings.__setitem__('start_date', '1900-01-01') %}
+{% endif %}
+{% set original_start_date = parse_comparison_value(date_settings.start_date) %}
+{% set end_date = 'CURRENT_DATE' if not date_settings.end_date else parse_comparison_value(date_settings.end_date) %}
+{% set time_grain = 'day' if not date_settings.time_grain else date_settings.time_grain %}
+{% if time_grain|lower == 'all' and secondary_calculations|length > 0 %}
+{{ raise_exception("Secondary calculations are not allowed when the time grain is 'all'") }}
+{% endif %}
+{% set start_date = adjust_start_date(start_date=date_settings.start_date, time_grain=time_grain, secondary_calculations=secondary_calculations) %}
+{% set unique_metrics = combine_metrics(unique_metrics) %}
 
 {% set table_metrics = {} %}
 {% set metric_names = [] %}
-{% set aggregations = [] %}
 
 {% if dimensions %}
 {% set source_tables = []|to_set %}
@@ -62,26 +65,14 @@
     date_filter,
     get_filter_statement(filters)
 ]) if start_date is defined and metrics[0].type|lower != 'expression' else filters %}
-
-{% set distinct_values = get_distinct_vals(
-    columns=dimensions,
-    target_metric=None,
-    max_vals=max_num_groups,
-    source_table=source_table,
-    filters=distinct_vals_filters
-) | from_json %}
-{% if distinct_values is not defined or not distinct_values %}
-    {{ raise_exception('This query returns 0 rows. Please adjust the inputs and try again.') }}
-{% endif %}
 {% endif %}
 
-{% set base_query %}
 with
-{# Expression Metrics #}
-{% for metric in expression_metrics %}
+{% for metric in unique_metrics %}
 {% do metric_names.extend(metric.names) %}
 {% do table_metrics.__setitem__('metric__' + metric.name, metric.names) %}
 metric__{{ metric.name }} as (
+    {# Expression Metrics #}
     {% if metric.type|lower == 'expression' %}
     {{ calculate_expression_metric_values(
         name=metric.name,
@@ -91,10 +82,10 @@ metric__{{ metric.name }} as (
         start_date=start_date,
         end_date=end_date,
         time_grain=time_grain,
-        distinct_values=distinct_values,
         filters=combine_filters(metric.filters, filters)
     ) | indent }}
     {% else %}
+    {# Normal Metrics #}
     {{ calculate_timeseries_metric_values(
         aggregations=[{
             'column': metric.target_expression,
@@ -107,15 +98,13 @@ metric__{{ metric.name }} as (
         end_date=end_date,
         time_grain=time_grain,
         source_table=metric.source_table,
-        filters=combine_filters(metric.filters, filters),
-        distinct_values=distinct_values
+        filters=combine_filters(metric.filters, filters)
     ) | indent }}
     {% endif %}
 ),
 {% endfor %}
 
 {# Join expression and aggregation metrics #}
-{% set dimensions = ['dimensions'] if dimensions %}
 {% set tables = table_metrics.keys()|list %}
 joined as (
     select
@@ -159,25 +148,3 @@ secondary_calculations as (
 select * from secondary_calculations
 where period_min >= {{ original_start_date }}
 order by {% for i in range(1, 3 + dimensions|length) %}{{ i }}{{ ',' if not loop.last else '\n' }}{% endfor %}
-{% set secondary_calculation_metrics = [] %}
-{% for calc_config in secondary_calculations %}
-{% set calc_metric_names = calc_config.metric_names or metric_names %}
-{% for metric_name in calc_metric_names %}
-{% do secondary_calculation_metrics.append(metric_name + '_' + calc_config.alias) %}
-{% endfor %}
-{% endfor %}
-{% do metric_names.extend(secondary_calculation_metrics) %}
-{% endset %}
-
-{% if dimensions %}
-{{ pivot_plot_values(
-    base_query=base_query,
-    x_axis='period',
-    metric_names=metric_names,
-    distinct_values=distinct_values,
-    axis_type=axis_type,
-    x_axis_order=x_axis_order
-) }}
-{% else %}
-{{ base_query }}
-{% endif %}
