29a67cabb802d600740475ff2cfe2ff464c57d5e
Kimball Hill <hillkimball@gmail.com>
2023-02-16 22:31:15 +0100

    update filter snippet (#371)

--- rasgotransforms/rasgotransforms/snippets/filter_list.sql
+++ rasgotransforms/rasgotransforms/snippets/filter_list.sql
@@ -1,24 +1,3 @@
 {% set filters = filter_list %}
-{% if filters %}
-{% set filters = {'filters': filters, 'logical_operator': 'and' } %}
-{% for filter in filters.filters %}
-    {% if filter is not string %}
-        {% if filter is mapping and 'compoundBoolean' in filter and filter['compoundBoolean'] %}
-            {% do filters.__setitem__('logical_operator', filter['compoundBoolean']) %}
-        {% endif %}
-    {% endif %}
-{% endfor %}
-(
-    {% for filter in filters.filters %}
-    {% if filter is not mapping %}
-    {{ filters.logical_operator + ' ' if not loop.first }}{{ filter }}
-    {% elif filter.operator|upper == 'CONTAINS' %}
-    {{ filters.logical_operator.value + ' ' if not loop.first }}{{ filter.columnName }} like '%{{ filter.comparisonValue }}%'
-    {% else %}
-    {{ filters.logical_operator + ' ' if not loop.first }}{{ filter.columnName }} {{ filter.operator }} {{ filter.comparisonValue }}
-    {% endif %}
-    {% endfor %}
-)
-{% else %}
-true
-{% endif %}
+{% from 'filter.sql' import get_filter_statement %}
+{{ get_filter_statement(filters) }}
